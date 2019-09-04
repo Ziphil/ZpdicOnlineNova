013c8377f56f1c5e53bf4abf248e454d6cf32488
Ziphil <yfe23457@nifty.com>
2019-09-04 21:42:24 +0900

    初期コミット

---
+++ source/index.ts
@@ -0,0 +1,18 @@
+//
+
+import * as express from "express"; 
+
+
+const PORT = 3000;
+
+function main(): void {
+  let app = express();
+  app.get("/", (req, res) => {
+    res.send("Hello World!!");
+  });
+  app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}!`);
+  });
+}
+
+main();
