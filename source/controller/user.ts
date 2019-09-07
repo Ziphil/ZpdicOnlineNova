a61e31f5722df0d4c2e4e248f726a3c66a96206e
Ziphil <yfe23457@nifty.com>
2019-09-07 12:58:35 +0900

    ユーザーログイン機構を作成

--- source/controller/user.ts
+++ source/controller/user.ts
@@ -15,6 +15,11 @@ import {
 
 export class UserController extends BaseController {
 
+  private getPage(request: Request, response: Response): void {
+    let name = request.session!.name;
+    response.render("user.ejs", {name});
+  }
+
   private getRegister(request: Request, response: Response): void {
     response.render("register.ejs");
   }
@@ -27,6 +32,28 @@ export class UserController extends BaseController {
     user.save().then(() => {
       response.send("Registered");
     });
+  }
+
+  private getLogin(request: Request, response: Response): void {
+    response.render("login.ejs");
+  }
+
+  private postLogin(request: Request, response: Response): void {
+    let name = request.body.name;
+    let password = request.body.password;
+    User.findOne({name, password}).exec().then((user) => {
+      if (user) {
+        request.session!.name = user.name;
+        response.send("Login succeeded");
+      } else {
+        response.send("Login failed");
+      }
+    });
+  }
+
+  private getLogout(request: Request, response: Response): void {
+    request.session!.destroy(() => null);
+    response.send("Logout");
   }
 
   private getDebug(request: Request, response: Response): void {
@@ -46,8 +73,12 @@ export class UserController extends BaseController {
 
   protected setup(): void {
     let router = this.router;
+    router.get("/", this.checkSession, this.getPage);
     router.get("/register", this.getRegister);
     router.post("/register", this.postRegister);
+    router.get("/login", this.getLogin);
+    router.post("/login", this.postLogin);
+    router.get("/logout", this.getLogout);
     router.get("/debug", this.getDebug);
   }
 
