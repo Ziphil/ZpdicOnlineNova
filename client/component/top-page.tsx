c423017afb0b2f9b8fa7ce07889c03a29d8aaf8b
Ziphil <yfe23457@nifty.com>
2020-02-28 01:13:13 +0900

    トップページに文章を追加

--- client/component/top-page.tsx
+++ client/component/top-page.tsx
@@ -24,6 +24,16 @@ export class TopPage extends Component {
           <Logo/>
           <LoginForm/>
         </div>
+        <div styleName="notice">
+          <p>
+            ZpDIC Online は、現在ベータ版として稼働しています。
+            内部的な仕様変更などにより、やむを得ず保存されているデータを削除する場合がありますので、ご了承ください。
+          </p>
+          <p>
+            意見や要望などは随時募集中です。
+            「このような機能がほしい」や「こうした方が使いやすい」などの意見がありましたら、Twitter を介して @Ziphil までご連絡ください。
+          </p>
+        </div>
       </div>
     );
   }
