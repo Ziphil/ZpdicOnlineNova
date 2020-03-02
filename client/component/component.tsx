4e966eaad28b71ee80a78b77f5f332fc63c0837f
Ziphil <yfe23457@nifty.com>
2020-03-02 09:45:25 +0900

    認証失敗時のステータスコードを変更

--- client/component/component.tsx
+++ client/component/component.tsx
@@ -11,11 +11,11 @@ import * as http from "/client/util/http";
 
 export class ComponentBase<P = {}, S = {}, Q = {}, N = any> extends Component<RouteComponentProps<Q> & P, S, N> {
 
-  // 発生した例外が HTTP リクエストの 403 エラーであった場合に、ログインページに遷移します。
+  // 発生した例外が HTTP リクエストの 401 エラーであった場合に、ログインページに遷移します。
   // それ以外の例外であった場合は、単にその例外を再発生させます。
   // 引数として例外値が渡されなかった場合は、無条件でログインページに遷移します。
   protected jumpLogin(error?: any): void {
-    if (error === undefined || error?.response?.status === 403) {
+    if (error === undefined || error?.response?.status === 401) {
       http.logout();
       this.props.history.push("/login");
     } else {
--- server/controller/middle.ts
+++ server/controller/middle.ts
@@ -16,22 +16,22 @@ const SECRET_KEY = "zpdic_secret";
 
 // リクエストのヘッダーに書き込まれたトークンを利用して認証を行います。
 // 認証に成功した場合、request オブジェクトの user プロパティにユーザーオブジェクトを書き込み、次の処理を行います。
-// 認証に失敗した場合、リダイレクト先が渡されていればそこにリダイレクトし、そうでなければステータスコード 403 を返して終了します。
+// 認証に失敗した場合、リダイレクト先が渡されていればそこにリダイレクトし、そうでなければステータスコード 401 を返して終了します。
 export function verifyToken(redirect?: string): RequestHandler {
   let handler = async function (request: Request, response: Response, next: NextFunction): Promise<void> {
     let fail = function (): void {
       if (redirect !== undefined) {
         response.redirect(redirect);
       } else {
-        response.sendStatus(403);
+        response.sendStatus(401);
       }
     };
     let token = request.headers.authorization;
     if (token !== undefined) {
       jwt.verify(token, SECRET_KEY, async (error, data) => {
         if (!error) {
-          let id = (data as any).id;
-          let user = await UserModel.findById(id);
+          let anyData = data as any;
+          let user = await UserModel.findById(anyData.id).exec();
           if (user) {
             request.user = user;
             next();
