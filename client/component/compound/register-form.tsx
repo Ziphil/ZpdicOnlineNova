f076cc448b04dcbf185ab3f6105111e7ca7c7607
Ziphil <yfe23457@nifty.com>
2020-03-02 10:56:26 +0900

    フロントエンドからユーザーの新規登録ができるよう変更

--- client/component/compound/register-form.tsx
+++ client/component/compound/register-form.tsx
@@ -16,6 +16,9 @@ import {
   ComponentBase
 } from "/client/component/component";
 import {
+  UserRegisterBody
+} from "/client/type/user";
+import {
   applyStyle
 } from "/client/util/decorator";
 import * as http from "/client/util/http";
@@ -28,7 +31,8 @@ class RegisterFormBase extends ComponentBase<Props, State> {
     name: "",
     email: "",
     password: "",
-    passwordRepeat: ""
+    passwordRepeat: "",
+    error: null
   };
 
   private async performRegister(event: MouseEvent<HTMLInputElement>): Promise<void> {
@@ -37,21 +41,56 @@ class RegisterFormBase extends ComponentBase<Props, State> {
     let password = this.state.password;
     let passwordRepeat = this.state.passwordRepeat;
     if (password === passwordRepeat) {
-      console.log("Not implemented");
+      let response = await http.post<UserRegisterBody>("/api/user/register", {name, email, password}, [400]);
+      let data = response.data;
+      if (!("error" in data)) {
+        let succeed = await http.login(name, password);
+        if (succeed) {
+          this.props.history.replace("/dashboard");
+        } else {
+          this.setState({error: "loginFailed"});
+        }
+      } else {
+        this.setState({error: data.error});
+      }
+    } else {
+      this.setState({error: "passwordMismatched"});
     }
   }
 
   public render(): ReactNode {
+    let errorNode;
+    let error = this.state.error;
+    if (error) {
+      let errorMessage = "予期せぬエラーです。";
+      if (error === "passwordMismatched") {
+        errorMessage = "確認用のパスワードが一致しません。";
+      } else if (error === "invalidName") {
+        errorMessage = "ユーザー名が不正です。ユーザー名は半角英数字とアンダーバーとハイフンのみで構成してください。";
+      } else if (error === "duplicatedName") {
+        errorMessage = "そのユーザー名はすでに存在しています。";
+      } else if (error === "loginFailed") {
+        errorMessage = "ログインに失敗しました。珍しいですね!";
+      }
+      errorNode = (
+        <ul styleName="error">
+          <li>{errorMessage}</li>
+        </ul>
+      );
+    }
     let node = (
-      <form styleName="login">
-        <Input label="ユーザー名" onChange={(value) => this.setState({name: value})}/>
-        <Input label="メールアドレス" onChange={(value) => this.setState({email: value})}/>
-        <Input label="パスワード" type="password" onChange={(value) => this.setState({password: value})}/>
-        <Input label="パスワード (確認)" type="password" onChange={(value) => this.setState({passwordRepeat: value})}/>
-        <div styleName="button-group">
-          <Button value="新規登録" onClick={this.performRegister.bind(this)}/>
-        </div>
-      </form>
+      <div>
+        {errorNode}
+        <form styleName="login">
+          <Input label="ユーザー名" onChange={(value) => this.setState({name: value})}/>
+          <Input label="メールアドレス" onChange={(value) => this.setState({email: value})}/>
+          <Input label="パスワード" type="password" onChange={(value) => this.setState({password: value})}/>
+          <Input label="パスワード (確認)" type="password" onChange={(value) => this.setState({passwordRepeat: value})}/>
+          <div styleName="button-group">
+            <Button value="新規登録" onClick={this.performRegister.bind(this)}/>
+          </div>
+        </form>
+      </div>
     );
     return node;
   }
@@ -65,7 +104,8 @@ type State = {
   name: string,
   email: string,
   password: string,
-  passwordRepeat: string
+  passwordRepeat: string,
+  error: string | null
 };
 
 export let RegisterForm = withRouter(RegisterFormBase);
