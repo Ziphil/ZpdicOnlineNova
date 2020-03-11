abc9fff92e76a5b00dd6c295eb3f8a3afc57770b
Ziphil <yfe23457@nifty.com>
2020-03-11 23:47:31 +0900

    MobX由来の独自デコレータを追加

--- client/util/decorator.ts
+++ client/util/decorator.ts
@@ -1,6 +1,23 @@
 //
 
+import {
+  action as mobxAction,
+  observable as mobxObservable
+} from "mobx";
+import {
+  inject as mobxInject,
+  observer as mobxObserver
+} from "mobx-react";
+import {
+  ComponentClass
+} from "react";
 import * as css from "react-css-modules";
+import {
+  RouteComponentProps
+} from "react-router";
+import {
+  withRouter
+} from "react-router-dom";
 
 
 export function applyStyle(component: any): ClassDecorator {
@@ -8,3 +25,21 @@ export function applyStyle(component: any): ClassDecorator {
   let decorator = css(component, options);
   return decorator;
 }
+
+export function route<P extends Partial<RouteComponentProps<any>>, T extends ComponentClass<P>>(clazz: T): T {
+  let anyClass = clazz as any;
+  let resultClass = withRouter(anyClass) as any;
+  return resultClass;
+}
+
+export function inject<P, T extends ComponentClass<P>>(clazz: T): T {
+  return mobxInject("store")(clazz);
+}
+
+export function observer<P, T extends ComponentClass<P>>(clazz: T): T {
+  return mobxObserver(clazz);
+}
+
+export let observable = mobxObservable;
+export let action = mobxAction;
+export let boundAction = mobxAction.bound;
