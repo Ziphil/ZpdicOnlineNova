08f53c9e4318c4208197271f1fbdb5e6229e226d
Ziphil <yfe23457@nifty.com>
2020-03-12 15:58:56 +0900

    更新時に別のページに勝手に飛ばされていたのを修正

--- client/component/root.tsx
+++ client/component/root.tsx
@@ -40,9 +40,12 @@ import {
 
 
 @applyStyle(require("./root.scss"))
-export class Root extends StoreComponent<{}, {}> {
+export class Root extends StoreComponent<Props, State> {
 
   private store: GlobalStore = new GlobalStore();
+  public state: State = {
+    ready: false
+  };
 
   public async componentDidMount(): Promise<void> {
     let response = await this.requestGet("fetchUserInfo", {}, true);
@@ -52,6 +55,7 @@ export class Root extends StoreComponent<{}, {}> {
     } else {
       this.store.user = null;
     }
+    this.setState({ready: true});
   }
 
   // 認証済みかどうかを確認し、その結果に応じて表示するコンポーネントを切り返るコンポーネントを返します。
@@ -68,22 +72,33 @@ export class Root extends StoreComponent<{}, {}> {
   }
 
   public render(): ReactNode {
-    return (
-      <BrowserRouter>
-        <Provider store={this.store}>
-          <Switch>
-            <GuestRoute exact path="/" redirect="/dashboard" component={TopPage}/>
-            <GuestRoute exact path="/login" redirect="/dashboard" component={LoginPage}/>
-            <GuestRoute exact path="/register" redirect="/dashboard" component={RegisterPage}/>
-            <PrivateRoute exact path="/dashboard/:mode" redirect="/login" component={DashboardPage}/>
-            <PrivateRoute exact path="/dashboard" redirect="/login" component={DashboardPage}/>
-            <Route exact path="/dictionary/list" component={DictionaryListPage}/>
-            <Route exact path="/dictionary/:number(\d+)" component={DictionaryPage}/>
-            <PrivateRoute exact path="/dictionary/setting/:number(\d+)" redirect="/login" component={DictionarySettingPage}/>
-          </Switch>
-        </Provider>
-      </BrowserRouter>
-    );
+    let node;
+    if (this.state.ready) {
+      node = (
+        <BrowserRouter>
+          <Provider store={this.store}>
+            <Switch>
+              <GuestRoute exact path="/" redirect="/dashboard" component={TopPage}/>
+              <GuestRoute exact path="/login" redirect="/dashboard" component={LoginPage}/>
+              <GuestRoute exact path="/register" redirect="/dashboard" component={RegisterPage}/>
+              <PrivateRoute exact path="/dashboard/:mode" redirect="/login" component={DashboardPage}/>
+              <PrivateRoute exact path="/dashboard" redirect="/login" component={DashboardPage}/>
+              <Route exact path="/dictionary/list" component={DictionaryListPage}/>
+              <Route exact path="/dictionary/:number(\d+)" component={DictionaryPage}/>
+              <PrivateRoute exact path="/dictionary/setting/:number(\d+)" redirect="/login" component={DictionarySettingPage}/>
+            </Switch>
+          </Provider>
+        </BrowserRouter>
+      );
+    }
+    return node;
   }
 
 }
+
+
+type Props = {
+};
+type State = {
+  ready: boolean
+};
