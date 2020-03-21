9f066ff609f0789db5ace7ea98d52df28679f070
Ziphil <yfe23457@nifty.com>
2020-03-21 19:38:17 +0900

    辞書のヘッダーのリンクでクエリ文字列を維持するよう変更

--- client/component/compound/dictionary-header.tsx
+++ client/component/compound/dictionary-header.tsx
@@ -2,38 +2,42 @@
 
 import * as react from "react";
 import {
-  Component,
   ReactNode
 } from "react";
 import {
   Link
 } from "/client/component/atom";
 import {
-  applyStyle
+  StoreComponent
+} from "/client/component/component";
+import {
+  applyStyle,
+  inject,
+  route
 } from "/client/component/decorator";
 import {
   SlimeDictionarySkeleton
 } from "/server/skeleton/dictionary/slime";
 
 
+@route @inject
 @applyStyle(require("./dictionary-header.scss"))
-export class DictionaryHeader extends Component<Props, State> {
+export class DictionaryHeader extends StoreComponent<Props, State> {
 
   public render(): ReactNode {
     let nameNode;
     if (this.props.dictionary) {
-      let href = "/dictionary/" + this.props.dictionary.number;
-      nameNode = (
-        <div styleName="name">
-          <Link label={this.props.dictionary.name} href={href} style="plane"/>
-        </div>
-      );
+      let queryString = this.props.location!.search;
+      let href = "/dictionary/" + this.props.dictionary.number + queryString;
+      nameNode = <Link label={this.props.dictionary.name} href={href} style="plane"/>;
     }
     let node = (
       <div styleName="root">
         <div styleName="container">
           <div styleName="left">
-            {nameNode}
+            <div styleName="name">
+              {nameNode}
+            </div>
           </div>
         </div>
       </div>
