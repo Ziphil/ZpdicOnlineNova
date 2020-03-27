dff05728f58ce608b0231e094faecac454b5cf6e
Ziphil <yfe23457@nifty.com>
2020-03-27 18:18:11 +0900

    リファクタリング

--- client/component/atom/radio-group.tsx
+++ client/component/atom/radio-group.tsx
@@ -19,15 +19,13 @@ import {
 @applyStyle(require("./radio-group.scss"))
 export class RadioGroup<V extends string> extends Component<Props<V>, State<V>> {
 
-  public state: State<V> = {
-    value: null
-  };
-
   public constructor(props: any) {
     super(props);
+    let value = null;
     if (this.props.initialValue !== undefined) {
-      this.state = {value: this.props.initialValue};
+      value = this.props.initialValue;
     }
+    this.state = {value};
   }
 
   private handleChange(event: ChangeEvent<HTMLInputElement>): void {
