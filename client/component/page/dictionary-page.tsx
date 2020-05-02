5982193eac8cab318929017082db3e1d8e78d5ae
Ziphil <yfe23457@nifty.com>
2020-05-02 21:18:47 +0900

    リファクタリング

--- client/component/page/dictionary-page.tsx
+++ client/component/page/dictionary-page.tsx
@@ -46,13 +46,13 @@ export class DictionaryPage extends StoreComponent<Props, State, Params> {
   public state: State = {
     dictionary: null,
     authorized: false,
-    showsExplanation: true,
-    hitSize: 0,
-    hitWords: [],
     search: "",
     mode: "both",
     type: "prefix",
-    page: 0
+    page: 0,
+    showsExplanation: true,
+    hitSize: 0,
+    hitWords: []
   };
 
   public constructor(props: any) {
@@ -196,34 +196,32 @@ export class DictionaryPage extends StoreComponent<Props, State, Params> {
     });
   }
 
-  public render(): ReactNode {
+  private renderWordListNode(): ReactNode {
     let maxPage = Math.max(Math.ceil(this.state.hitSize / 40) - 1, 0);
-    let wordListNode;
-    if (this.state.dictionary) {
-      if (this.state.showsExplanation) {
-        wordListNode = (
-          <Markdown source={this.state.dictionary.explanation}/>
-        );
-      } else {
-        wordListNode = (
-          <Fragment>
-            <div styleName="word-list">
-              <WordList dictionary={this.state.dictionary} words={this.state.hitWords} authorized={this.state.authorized} offset={0} size={40}/>
-            </div>
-            <div styleName="pagination-button">
-              <PaginationButton page={this.state.page} minPage={0} maxPage={maxPage} onSet={this.handlePageSet.bind(this)}/>
-            </div>
-          </Fragment>
-        );
-      }
-    }
+    let node = (
+      <Fragment>
+        <div styleName="word-list">
+          <WordList dictionary={this.state.dictionary!} words={this.state.hitWords} authorized={this.state.authorized} offset={0} size={40}/>
+        </div>
+        <div styleName="pagination-button">
+          <PaginationButton page={this.state.page} minPage={0} maxPage={maxPage} onSet={this.handlePageSet.bind(this)}/>
+        </div>
+      </Fragment>
+    );
+    return node;
+  }
+
+  public render(): ReactNode {
+    let innerNode = (this.state.dictionary !== null) && (
+      (this.state.showsExplanation) ? <Markdown source={this.state.dictionary.explanation}/> : this.renderWordListNode()
+    );
     let node = (
       <Page showsDictionary={true} showsDictionarySetting={this.state.authorized} dictionary={this.state.dictionary}>
         <div styleName="search-form">
           <SearchForm search={this.state.search} mode={this.state.mode} type={this.state.type} onSomeSet={this.handleSomeSearchSet.bind(this)}/>
         </div>
         <Loading loading={this.state.dictionary === null}>
-          {wordListNode}
+          {innerNode}
         </Loading>
       </Page>
     );
@@ -238,13 +236,13 @@ type Props = {
 type State = {
   dictionary: SlimeDictionarySkeleton | null,
   authorized: boolean,
-  showsExplanation: boolean,
-  hitSize: number,
-  hitWords: Array<SlimeWordSkeleton>,
   search: string,
   mode: SearchMode,
   type: SearchType
-  page: number
+  page: number,
+  showsExplanation: boolean,
+  hitSize: number,
+  hitWords: Array<SlimeWordSkeleton>
 };
 type Params = {
   value: string
