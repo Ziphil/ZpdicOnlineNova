986ab75037b3925ce708cd6729a876aed3717611
Ziphil <yfe23457@nifty.com>
2020-06-17 17:38:45 +0900

    文字列リテラル型のユーティリティクラスの実装を変更

--- server/util/literal-type.ts
+++ server/util/literal-type.ts
@@ -3,29 +3,54 @@
 
 export class LiteralUtilType<T extends string> {
 
+  private values: Array<T>;
   public defaultValue: T;
-  public is: (value: string) => value is T;
 
-  private constructor(defaultValue: T, is: (value: string) => value is T) {
+  private constructor(values: Array<T>, defaultValue: T) {
+    this.values = values;
     this.defaultValue = defaultValue;
-    this.is = is;
   }
 
-  public cast(value: string): T {
-    if (this.is(value)) {
-      return value;
+  public is(value: string | null | undefined): value is T {
+    if (typeof value === "string") {
+      let castValue = value as T;
+      let index = this.values.indexOf(castValue);
+      return index >= 0;
+    } else {
+      return false;
+    }
+  }
+
+  public cast(value: string | number | null | undefined): T {
+    if (typeof value === "string") {
+      let castValue = value as T;
+      let index = this.values.indexOf(castValue);
+      if (index >= 0) {
+        return this.values[index];
+      } else {
+        return this.defaultValue;
+      }
+    } else if (typeof value === "number") {
+      if (value >= 0 && value < this.values.length) {
+        return this.values[value];
+      } else {
+        return this.defaultValue;
+      }
     } else {
       return this.defaultValue;
     }
   }
 
+  public indexOf(value: string): number {
+    let anyValue = value as any;
+    let index = this.values.indexOf(anyValue);
+    return index;
+  }
+
   public static create<T extends string>(values: {0: T} & ArrayLike<T>): LiteralUtilType<T> {
+    let castValues = Array.from(values);
     let defaultValue = values[0];
-    let is = function (value: string): value is T {
-      let anyValues = values as any;
-      return anyValues.indexOf(value) >= 0;
-    };
-    let result = new LiteralUtilType(defaultValue, is);
+    let result = new LiteralUtilType(castValues, defaultValue);
     return result;
   }
 
