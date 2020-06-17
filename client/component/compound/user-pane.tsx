c3233d420d6735423991f596a9138a34b6847578
Ziphil <yfe23457@nifty.com>
2020-06-18 04:07:44 +0900

    ユーザーの編集権限を削除する際にアラートを出すよう変更

--- client/component/compound/user-pane.tsx
+++ client/component/compound/user-pane.tsx
@@ -2,10 +2,12 @@
 
 import * as react from "react";
 import {
+  Fragment,
   MouseEvent,
   ReactNode
 } from "react";
 import {
+  Alert,
   Button
 } from "/client/component/atom";
 import {
@@ -31,6 +33,10 @@ import {
 @applyStyle(require("./user-pane.scss"))
 export class UserPane extends StoreComponent<Props, State> {
 
+  public state: State = {
+    alertOpen: false
+  };
+
   private async deleteAuthorizedUser(event: MouseEvent<HTMLButtonElement>): Promise<void> {
     if (this.props.dictionary !== undefined) {
       let number = this.props.dictionary.number;
@@ -46,23 +52,38 @@ export class UserPane extends StoreComponent<Props, State> {
   }
 
   public render(): ReactNode {
+    let alertText = `
+      このユーザーの編集権限を解除します。
+      本当によろしいですか?
+    `;
     let node = (
-      <WhitePane clickable={false}>
-        <div>
-          <div styleName="head">
-            <div styleName="right">
-              <div styleName="image"/>
-            </div>
-            <div styleName="left">
-              <div styleName="screen-name">@{this.props.user.name}</div>
-              <div styleName="name">@{this.props.user.name}</div>
+      <Fragment>
+        <WhitePane clickable={false}>
+          <div>
+            <div styleName="head">
+              <div styleName="right">
+                <div styleName="image"/>
+              </div>
+              <div styleName="left">
+                <div styleName="screen-name">@{this.props.user.name}</div>
+                <div styleName="name">@{this.props.user.name}</div>
+              </div>
             </div>
           </div>
-        </div>
-        <div styleName="setting">
-          <Button label="解除" iconLabel="&#xF05E;" style="caution" reactive={true} onClick={this.deleteAuthorizedUser.bind(this)}/>
-        </div>
-      </WhitePane>
+          <div styleName="setting">
+            <Button label="解除" iconLabel="&#xF05E;" style="caution" reactive={true} onClick={() => this.setState({alertOpen: true})}/>
+          </div>
+        </WhitePane>
+        <Alert
+          text={alertText}
+          iconLabel="&#xF071;"
+          confirmLabel="解除"
+          open={this.state.alertOpen}
+          outsideClosable={true}
+          onClose={() => this.setState({alertOpen: false})}
+          onConfirm={this.deleteAuthorizedUser.bind(this)}
+        />
+      </Fragment>
     );
     return node;
   }
@@ -76,4 +97,5 @@ type Props = {
   onSubmit?: (event: MouseEvent<HTMLButtonElement>) => void | Promise<void>
 };
 type State = {
+  alertOpen: boolean
 };
