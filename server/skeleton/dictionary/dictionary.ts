b111d5cd5de948bbf095038d353fc1d0ffbb2d2c
Ziphil <yfe23457@nifty.com>
2020-09-05 19:03:26 +0900

    使わなくなったメソッドを削除

--- server/skeleton/dictionary/dictionary.ts
+++ server/skeleton/dictionary/dictionary.ts
@@ -4,9 +4,6 @@ import {
   DictionaryAuthority
 } from "/server/model/dictionary";
 import {
-  NormalSearchParameter
-} from "/server/model/search-parameter";
-import {
   Word
 } from "/server/skeleton/dictionary";
 import {
@@ -36,78 +33,6 @@ export class DetailedDictionary extends Dictionary {
   public words?: Array<Word>;
   public wordSize!: number;
   public user!: User;
-
-  public search(parameter: NormalSearchParameter): Array<Word> {
-    let search = parameter.search;
-    let mode = parameter.mode;
-    let type = parameter.type;
-    let escapedSearch = search.replace(/[\\^$.*+?()[\]{}|]/g, "\\$&");
-    let hitWords = this.words?.filter((word) => {
-      let createTargets = function (innerMode: string): Array<string> {
-        let targets = [];
-        if (innerMode === "name") {
-          targets.push(word.name);
-        } else if (innerMode === "equivalent") {
-          for (let equivalent of word.equivalents) {
-            targets.push(...equivalent.names);
-          }
-        } else if (innerMode === "information") {
-          for (let information of word.informations) {
-            targets.push(information.text);
-          }
-        }
-        return targets;
-      };
-      let createNeedle = function (innerType: string): RegExp {
-        let needle;
-        if (innerType === "exact") {
-          needle = new RegExp("^" + escapedSearch + "$");
-        } else if (innerType === "prefix") {
-          needle = new RegExp("^" + escapedSearch);
-        } else if (innerType === "suffix") {
-          needle = new RegExp(escapedSearch + "$");
-        } else if (innerType === "part") {
-          needle = new RegExp(escapedSearch);
-        } else if (innerType === "regular") {
-          try {
-            needle = new RegExp(search);
-          } catch (error) {
-            needle = /^$/;
-          }
-        } else {
-          needle = /^$/;
-        }
-        return needle;
-      };
-      let createPredicate = function (innerMode: string, innerType: string): boolean {
-        let targets = createTargets(innerMode);
-        let needle = createNeedle(innerType);
-        let predicate = targets.some((target) => {
-          return !!target.match(needle);
-        });
-        return predicate;
-      };
-      let finalPredicate;
-      if (mode === "name") {
-        finalPredicate = createPredicate("name", type);
-      } else if (mode === "equivalent") {
-        finalPredicate = createPredicate("equivalent", type);
-      } else if (mode === "content") {
-        let namePredicate = createPredicate("name", type);
-        let equivalentPredicate = createPredicate("equivalent", type);
-        let informationPredicate = createPredicate("information", type);
-        finalPredicate = namePredicate || equivalentPredicate || informationPredicate;
-      } else if (mode === "both") {
-        let namePredicate = createPredicate("name", type);
-        let equivalentPredicate = createPredicate("equivalent", type);
-        finalPredicate = namePredicate || equivalentPredicate;
-      } else {
-        finalPredicate = false;
-      }
-      return finalPredicate;
-    });
-    return hitWords ?? [];
-  }
 
 }
 
