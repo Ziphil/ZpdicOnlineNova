554bf30507434781a123ab86421fed0c8b1009f6
Ziphil <yfe23457@nifty.com>
2020-09-25 13:03:47 +0900

    不要な環境変数の読み込みを削除

--- server/index.ts
+++ server/index.ts
@@ -33,9 +33,8 @@ import {
 dotenv.config({path: "./variable.env"});
 
 export const PORT = process.env["PORT"] || 8050;
-export const MONGO_URI = process.env["DB_URI"] || "mongodb://localhost:27017/zpdic";
+export const MONGO_URI = process.env["DB_URI"] || "mongodb://dummy";
 export const COOKIE_SECRET = process.env["COOKIE_SECRET"] || "cookie-zpdic";
-export const SESSION_SECRET = process.env["SESSION_SECRET"] || "session-zpdic";
 export const JWT_SECRET = process.env["JWT_SECRET"] || "jwt-secret";
 export const SENDGRID_KEY = process.env["SENDGRID_KEY"] || "dummy";
 export const RECAPTCHA_SECRET = process.env["RECAPTCHA_SECRET"] || "dummy";
