52f8172bb3a4def1e1d1c740c4a52735a70879c0
Ziphil <yfe23457@nifty.com>
2020-10-30 20:19:20 +0900

    リファクタリング

--- client/component/store.ts
+++ client/component/store.ts
@@ -35,7 +35,7 @@ export class GlobalStore {
   public async changeLocale(locale: string): Promise<void> {
     let language = LANGUAGES.find((language) => language.locale === locale) ?? LANGUAGES[0];
     this.locale = locale;
-    this.messages = await language.fetchMessages();
+    this.messages = await language.fetchMessages().then((module) => module.default);
     localStorage.setItem("locale", locale);
   }
 
