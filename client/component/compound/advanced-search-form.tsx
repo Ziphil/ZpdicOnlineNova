05da91ed01806ec210144f93a8e195c314358547
Ziphil <yfe23457@nifty.com>
2020-11-05 15:35:32 +0900

    高度な検索のUIの基盤を作成

--- client/component/compound/advanced-search-form.tsx
+++ client/component/compound/advanced-search-form.tsx
@@ -14,8 +14,13 @@ import {
   style
 } from "/client/component/decorator";
 import {
+  deleteAt
+} from "/client/util/misc";
+import {
   AdvancedSearchParameter,
+  AdvancedSearchParameterElement,
   SEARCH_MODES,
+  SEARCH_TYPES,
   SearchParameter
 } from "/server/skeleton/dictionary";
 
@@ -33,21 +38,45 @@ export default class AdvancedSearchForm extends Component<Props, State> {
     }
   }
 
+  private setParameter<T extends Array<any>>(setter: (...args: T) => void): (...args: T) => void {
+    let outerThis = this;
+    let wrapper = function (...args: T): void {
+      setter(...args);
+      let parameter = outerThis.state.parameter;
+      outerThis.setState({parameter});
+    };
+    return wrapper;
+  }
+
   public render(): ReactNode {
+    let parameter = this.state.parameter;
     let searchNodes = this.state.parameter.elements.map((element, index) => {
       let modeSpecs = SEARCH_MODES.map((mode) => ({value: mode, text: this.trans(`advancedSearchForm.${mode}`)}));
+      let typeSpecs = SEARCH_TYPES.map((type) => ({value: type, text: this.trans(`advancedSearchForm.${type}`)}));
+      let modeLabel = (index === 0) ? this.trans("advancedSearchForm.mode") : undefined;
+      let titleLabel = (index === 0) ? this.trans("advancedSearchForm.title") : undefined;
+      let searchLabel = (index === 0) ? this.trans("advancedSearchForm.search") : undefined;
+      let typeLabel = (index === 0) ? this.trans("advancedSearchForm.type") : undefined;
       let searchNode = (
-        <div styleName="form" key={index}>
-          <Selection value={element.mode} label={this.trans("advancedSearchForm.mode")} specs={modeSpecs}/>
+        <div styleName="inner" key={index}>
+          <div styleName="form">
+            <Selection value={element.mode} label={modeLabel} specs={modeSpecs}/>
+            <Input value={element.title} label={titleLabel}/>
+            <Input value={element.search} label={searchLabel}/>
+            <Selection value={element.type} label={typeLabel} specs={typeSpecs}/>
+          </div>
+          <div styleName="button">
+            <Button iconLabel="&#xF00D;" onClick={this.setParameter(() => deleteAt(parameter.elements, index))}/>
+          </div>
         </div>
       );
       return searchNode;
     });
     let node = (
-      <Overlay size="large" title={this.trans("advancedSearchForm.title")} open={this.props.open} onClose={this.handleClose.bind(this)}>
+      <Overlay size="large" title={this.trans("advancedSearchForm.overlayTitle")} open={this.props.open} onClose={this.handleClose.bind(this)}>
         {searchNodes}
         <div styleName="plus">
-          <Button iconLabel="&#xF067;"/>
+          <Button iconLabel="&#xF067;" onClick={this.setParameter(() => parameter.elements.push(AdvancedSearchParameterElement.createEmpty()))}/>
         </div>
       </Overlay>
     );
--- server/skeleton/dictionary/search-parameter/advanced-search-parameter.ts
+++ server/skeleton/dictionary/search-parameter/advanced-search-parameter.ts
@@ -19,8 +19,17 @@ export class AdvancedSearchParameter extends Skeleton {
 export class AdvancedSearchParameterElement extends Skeleton {
 
   public search!: string;
-  public title!: string | null;
+  public title!: string;
   public mode!: SearchMode;
   public type!: SearchType;
+
+  public static createEmpty(): AdvancedSearchParameterElement {
+    let search = "";
+    let title = "";
+    let mode = "name" as const;
+    let type = "exact" as const;
+    let skeleton = AdvancedSearchParameterElement.of({search, title, mode, type});
+    return skeleton;
+  }
 
 }
