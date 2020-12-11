e59e1a71a66fdefe191b3542d791070a52ce2f23
Ziphil <yfe23457@nifty.com>
2020-12-12 06:53:09 +0900

    ドロップダウンの表示制御をスクリプトで行うよう変更 (close #27)

--- client/component/atom/dropdown.tsx
+++ client/component/atom/dropdown.tsx
@@ -14,6 +14,14 @@ import {
 @style(require("./dropdown.scss"))
 export default class Dropdown<V> extends Component<Props<V>, State<V>> {
 
+  public static defaultProps: DefaultProps = {
+    open: false,
+    openByFocus: true
+  };
+  public state: State<V> = {
+    open: false
+  };
+
   private handleMouseDown(value: V, event: MouseEvent<HTMLDivElement>): void {
     if (this.props.onClick) {
       this.props.onClick(event);
@@ -24,6 +32,7 @@ export default class Dropdown<V> extends Component<Props<V>, State<V>> {
   }
 
   public render(): ReactNode {
+    let open = (this.props.openByFocus) ? this.state.open : this.props.open;
     let itemNodes = this.props.specs.map((spec, index) => {
       let itemNode = (
         <div styleName="suggestion-item" key={index} tabIndex={0} onMouseDown={(event) => this.handleMouseDown(spec.value, event)}>
@@ -32,14 +41,16 @@ export default class Dropdown<V> extends Component<Props<V>, State<V>> {
       );
       return itemNode;
     });
-    let suggestionNode = (this.props.specs.length > 0) && (
+    let suggestionNode = (open && this.props.specs.length > 0) && (
       <div styleName="suggestion">
         {itemNodes}
       </div>
     );
     let node = (
-      <div styleName="root" className={this.props.className}>
-        {this.props.children}
+      <div styleName="root" className={this.props.className} onFocus={() => this.setState({open: true})} onBlur={() => this.setState({open: false})}>
+        <div>
+          {this.props.children}
+        </div>
         {suggestionNode}
       </div>
     );
@@ -50,12 +61,20 @@ export default class Dropdown<V> extends Component<Props<V>, State<V>> {
 
 
 type Props<V> = {
-  specs: Array<DropdownSpec<V>>
+  specs: Array<DropdownSpec<V>>,
+  open: boolean,
+  openByFocus: boolean,
   onClick?: (event: MouseEvent<HTMLDivElement>) => void,
+  onClose?: () => void,
   onSet?: (value: V) => void,
   className?: string
 };
+type DefaultProps = {
+  open: boolean,
+  openByFocus: boolean
+};
 type State<V> = {
+  open: boolean
 };
 
 export type DropdownSpec<V> = {value: V, node: ReactNode};
