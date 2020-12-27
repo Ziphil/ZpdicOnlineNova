c7169228a6b3d872d6390052b60755b167b64bfb
Ziphil <yfe23457@nifty.com>
2020-12-27 09:30:21 +0900

    リソースの削除ができるよう変更

--- client/component/compound/resource-pane.tsx
+++ client/component/compound/resource-pane.tsx
@@ -2,8 +2,14 @@
 
 import * as react from "react";
 import {
+  Fragment,
+  MouseEvent,
   ReactNode
 } from "react";
+import {
+  AsyncOrSync
+} from "ts-essentials";
+import Alert from "/client/component/atom/alert";
 import Button from "/client/component/atom/button";
 import Label from "/client/component/atom/label";
 import Component from "/client/component/component";
@@ -21,9 +27,24 @@ import {
 @style(require("./resource-pane.scss"))
 export default class ResourcePane extends Component<Props, State> {
 
+  public state: State = {
+    alertOpen: false
+  };
   public static defaultProps: DefaultProps = {
     showCode: false
   };
+
+  private async discardResource(event: MouseEvent<HTMLButtonElement>): Promise<void> {
+    let number = this.props.dictionary.number;
+    let name = this.props.resource;
+    let response = await this.request("discardResource", {number, name});
+    if (response.status === 200) {
+      this.props.store!.addInformationPopup("resourceDiscarded");
+      if (this.props.onDiscardConfirm) {
+        await this.props.onDiscardConfirm(event);
+      }
+    }
+  }
 
   public render(): ReactNode {
     let url = AwsUtil.getFileUrl(`resource/${this.props.dictionary.number}/${this.props.resource}`);
@@ -39,18 +60,28 @@ export default class ResourcePane extends Component<Props, State> {
       </div>
     );
     let node = (
-      <div styleName="root">
-        {this.props.resource}
-        {codeNode}
-        <div styleName="button-wrapper">
-          <div styleName="image">
-            <img src={url}/>
-          </div>
-          <div styleName="button">
-            <Button label={this.trans("resourcePane.discard")} iconLabel="&#xF2ED;" style="simple"/>
+      <Fragment>
+        <div styleName="root">
+          {this.props.resource}
+          {codeNode}
+          <div styleName="button-wrapper">
+            <div styleName="image">
+              <img src={url}/>
+            </div>
+            <div styleName="button">
+              <Button label={this.trans("resourcePane.discard")} iconLabel="&#xF2ED;" style="simple" onClick={() => this.setState({alertOpen: true})}/>
+            </div>
           </div>
         </div>
-      </div>
+        <Alert
+          text={this.trans("resourcePane.alert")}
+          confirmLabel={this.trans("resourcePane.discard")}
+          open={this.state.alertOpen}
+          outsideClosable={true}
+          onClose={() => this.setState({alertOpen: false})}
+          onConfirm={this.discardResource.bind(this)}
+        />
+      </Fragment>
     );
     return node;
   }
@@ -61,10 +92,12 @@ export default class ResourcePane extends Component<Props, State> {
 type Props = {
   dictionary: Dictionary,
   resource: string,
-  showCode: boolean
+  showCode: boolean,
+  onDiscardConfirm?: (event: MouseEvent<HTMLButtonElement>) => AsyncOrSync<void>
 };
 type DefaultProps = {
   showCode: boolean
 };
 type State = {
+  alertOpen: boolean
 };
