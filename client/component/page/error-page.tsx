215413e225ddd8529d13ba7c69aa73db2011f022
Ziphil <yfe23457@nifty.com>
2021-01-25 10:10:54 +0900

    エラーページを作成

--- client/component/page/error-page.tsx
+++ client/component/page/error-page.tsx
@@ -14,11 +14,26 @@ import Page from "/client/component/page/page";
 @style(require("./error-page.scss"))
 export default class ErrorPage extends Component<Props, State> {
 
+  private getMessage(): string {
+    let error = this.props.error;
+    if (error instanceof Error) {
+      let message = error.message + "\n" + error.stack;
+      return message;
+    } else {
+      return error.toString();
+    }
+  }
+
   public render(): ReactNode {
     let node = (
       <Page>
         <div styleName="root">
-          Weird
+          <div styleName="description">
+            {this.trans("errorPage.description")}
+          </div>
+          <pre styleName="message">
+            {this.getMessage()}
+          </pre>
         </div>
       </Page>
     );
