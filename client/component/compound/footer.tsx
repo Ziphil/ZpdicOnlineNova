4bb07b62c9910f69d716a6589a37cc4cbbfa5a5a
Ziphil <yfe23457@nifty.com>
2021-03-07 22:45:39 +0900

    フッターの内容を変更

--- client/component/compound/footer.tsx
+++ client/component/compound/footer.tsx
@@ -4,6 +4,7 @@ import * as react from "react";
 import {
   ReactNode
 } from "react";
+import Link from "/client/component/atom/link";
 import Component from "/client/component/component";
 import {
   style
@@ -14,6 +15,7 @@ import {
 export default class Footer extends Component<Props, State> {
 
   public render(): ReactNode {
+    let styles = this.props.styles!;
     let date = new Date();
     let yearString = date.getFullYear().toString();
     let node = (
@@ -21,12 +23,15 @@ export default class Footer extends Component<Props, State> {
         <div styleName="container">
           <div styleName="left">
             <div styleName="copyright">
-              © {yearString} Ziphil
+              © 2020–{yearString} Ziphil<br/>
+              <Link className={styles["link"]} href="/document/other/privacy" style="plane">Privacy Policy</Link> · <Link className={styles["link"]} href="/contact" style="plane">Contact</Link>
             </div>
           </div>
           <div styleName="right">
-            This site is protected by reCAPTCHA.<br/>
-            The Google <a href="https://policies.google.com/privacy" target="_blank">Privacy Policy</a> and <a href="https://policies.google.com/terms" target="_blank">Terms of Service</a> apply.
+            <div styleName="copyright">
+              This site is protected by reCAPTCHA.<br/>
+              The Google <a href="https://policies.google.com/privacy" target="_blank">Privacy Policy</a> and <a href="https://policies.google.com/terms" target="_blank">Terms of Service</a> apply.
+            </div>
           </div>
         </div>
       </footer>
