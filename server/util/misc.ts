fcdb6c3213c362cb836dc99f4f70bbee5bae38b0
Ziphil <yfe23457@nifty.com>
2021-06-21 21:59:12 +0900

    コメントを追加

--- server/util/misc.ts
+++ server/util/misc.ts
@@ -6,6 +6,8 @@ import {
 import removeMarkdownOriginal from "remove-markdown";
 
 
+// 与えられた長さのランダムな URL-safe Base64 文字列を生成します。
+// addDate として true を渡すと、生成した文字列の後に 13 文字の日付データが追加されます。
 export function createRandomString(length: number, addDate?: boolean): string {
   let string = randomBytes(length).toString("base64").substring(0, length).replace(/\+/g, "-").replace(/\//g, "_");
   if (addDate) {
