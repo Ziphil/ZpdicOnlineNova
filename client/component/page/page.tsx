72ad5547e7bee659c589c7098f99c4f43c9bd4c3
Ziphil <yfe23457@nifty.com>
2022-02-02 03:46:50 +0900

    不要な記述を削除

--- client/component/page/page.tsx
+++ client/component/page/page.tsx
@@ -3,13 +3,11 @@
 import * as react from "react";
 import {
   ReactElement,
-  ReactNode,
-  useState
+  ReactNode
 } from "react";
 import {
   Helmet
 } from "react-helmet";
-import Drawer from "/client/component/atom/drawer";
 import DictionaryHeader from "/client/component/compound/dictionary-header";
 import Footer from "/client/component/compound/footer";
 import Header from "/client/component/compound/header";
@@ -42,8 +40,6 @@ const Page = create(
     showSettingLink?: boolean,
     children?: ReactNode
   }): ReactElement {
-
-    let [editerOpen, setEditorOpen] = useState(false);
 
     let spacerStyleName = StyleNameUtil.create(
       "spacer",
