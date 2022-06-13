d2d4fabad1d0d95da8eabd4d09226a5ba28c9fbe
Ziphil <yfe23457@nifty.com>
2022-06-13 22:54:01 +0900

    プリフェッチの設定を追加

--- client/component/page/loader.ts
+++ client/component/page/loader.ts
@@ -12,20 +12,19 @@ import {
 } from "/client/language";
 
 
-export async function loadTopPage(): Promise<{}> {
+export async function loadDashboardPage({}: RouteMatch<DefaultGenerics>): Promise<{}> {
   const promises = [
-    prefetchQuery("fetchNotifications", {offset: 0, size: 1}),
-    prefetchQuery("fetchOverallAggregation", {})
+    prefetchQuery("fetchDictionaries", {}),
+    prefetchQuery("fetchInvitations", {type: "edit"}),
+    prefetchQuery("fetchInvitations", {type: "transfer"})
   ];
   await Promise.all(promises);
   return {};
 }
 
-export async function loadDashboardPage(): Promise<{}> {
+export async function loadDictionaryListPage({}: RouteMatch<DefaultGenerics>): Promise<{}> {
   const promises = [
-    prefetchQuery("fetchDictionaries", {}),
-    prefetchQuery("fetchInvitations", {type: "edit"}),
-    prefetchQuery("fetchInvitations", {type: "transfer"})
+    prefetchQuery("fetchAllDictionaries", {order: "updatedDate", offset: 0, size: 20})
   ];
   await Promise.all(promises);
   return {};
@@ -45,6 +44,35 @@ export async function loadDictionarySettingPage({params}: RouteMatch<DefaultGene
 export async function loadDocumentPage({params}: RouteMatch<DefaultGenerics>): Promise<{}> {
   const path = ((params.firstPath) ? params.firstPath : "") + ((params.secondPath) ? "/" + params.secondPath : "");
   const promises = LANGUAGES.map(({locale}) => prefetchQuery("fetchDocument", {path, locale}));
+  await Promise.all(promises);
+  return {};
+}
+
+export async function loadExamplePage({params}: RouteMatch<DefaultGenerics>): Promise<{}> {
+  const number = +params.number;
+  const promises = [
+    prefetchQuery("fetchDictionary", {number}),
+    prefetchQuery("fetchExamples", {number, offset: 0, size: 40}),
+    prefetchQuery("checkDictionaryAuthorizationBoolean", {number, authority: "own"}),
+    prefetchQuery("checkDictionaryAuthorizationBoolean", {number, authority: "edit"})
+  ];
+  await Promise.all(promises);
+  return {};
+}
+
+export async function loadNotificationPage({}: RouteMatch<DefaultGenerics>): Promise<{}> {
+  const promises = [
+    prefetchQuery("fetchNotifications", {offset: 0, size: 10})
+  ];
+  await Promise.all(promises);
+  return {};
+}
+
+export async function loadTopPage({}: RouteMatch<DefaultGenerics>): Promise<{}> {
+  const promises = [
+    prefetchQuery("fetchNotifications", {offset: 0, size: 1}),
+    prefetchQuery("fetchOverallAggregation", {})
+  ];
   await Promise.all(promises);
   return {};
 }
--- client/component/root.tsx
+++ client/component/root.tsx
@@ -43,8 +43,11 @@ import InnerRoot from "/client/component/inner-root";
 import ErrorPage from "/client/component/page/error-page";
 import {
   loadDashboardPage,
+  loadDictionaryListPage,
   loadDictionarySettingPage,
   loadDocumentPage,
+  loadExamplePage,
+  loadNotificationPage,
   loadTopPage
 } from "/client/component/page/loader";
 import LoadingPage from "/client/component/page/loading-page";
@@ -69,9 +72,9 @@ const routes = [
   ...createRoute("/dashboard/dictionary/:number", () => import("/client/component/page/dictionary-setting-page"), {type: "private", redirect: "/login", loader: loadDictionarySettingPage}),
   ...createRoute("/dashboard", () => import("/client/component/page/dashboard-page"), {type: "private", redirect: "/login", loader: loadDashboardPage}),
   ...createRoute("/dictionary/:value", () => import("/client/component/page/dictionary-page"), {type: "none"}),
-  ...createRoute("/example/:number", () => import("/client/component/page/example-page"), {type: "none"}),
-  ...createRoute("/list", () => import("/client/component/page/dictionary-list-page"), {type: "none"}),
-  ...createRoute("/notification", () => import("/client/component/page/notification-page"), {type: "none"}),
+  ...createRoute("/example/:number", () => import("/client/component/page/example-page"), {type: "none", loader: loadExamplePage}),
+  ...createRoute("/list", () => import("/client/component/page/dictionary-list-page"), {type: "none", loader: loadDictionaryListPage}),
+  ...createRoute("/notification", () => import("/client/component/page/notification-page"), {type: "none", loader: loadNotificationPage}),
   ...createRoute("/contact", () => import("/client/component/page/contact-page"), {type: "none"}),
   ...createRoute("/document/:firstPath/:secondPath", () => import("/client/component/page/document-page"), {type: "none", loader: loadDocumentPage}),
   ...createRoute("/document/:firstPath", () => import("/client/component/page/document-page"), {type: "none", loader: loadDocumentPage}),
