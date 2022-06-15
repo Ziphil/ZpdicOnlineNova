41fdaa07fa4b19679d663b4cb4775e275708f1c1
Ziphil <yfe23457@nifty.com>
2022-06-15 23:07:09 +0900

    GA用のユーザー情報を送信するよう変更

--- client/component/hook/user.ts
+++ client/component/hook/user.ts
@@ -13,6 +13,10 @@ import {
   DetailedUser
 } from "/client/skeleton/user";
 import {
+  GtagUtil
+} from "/client/util/gtag";
+import {
+  ResponseData,
   SERVER_PATHS,
   SERVER_PATH_PREFIX
 } from "/server/controller/internal/type";
@@ -20,31 +24,33 @@ import {
 
 export const useRawMe = createGlobalState<DetailedUser | null>(null);
 
-export function useDefaultMe(): {user: DetailedUser | null, ready: boolean} {
-  const [user, setUser] = useRawMe();
+export function useDefaultMe(): {me: DetailedUser | null, ready: boolean} {
+  const [me, setMe] = useRawMe();
   const [ready, setReady] = useState(false);
   useMount(async () => {
     const url = SERVER_PATH_PREFIX + SERVER_PATHS["fetchUser"];
-    const response = await axios.post(url, {}, {validateStatus: () => true});
-    if (response.status === 200) {
-      const user = response.data;
-      setUser(user);
+    const response = await axios.post<ResponseData<"fetchUser">>(url, {}, {validateStatus: () => true});
+    if (response.status === 200 && !("error" in response.data)) {
+      const me = response.data;
+      GtagUtil.set("user_properties", [["id", me.id], ["name", me.name], ["screen_name", me.screenName]]);
+      setMe(me);
       setReady(true);
     } else {
-      setUser(null);
+      setMe(null);
       setReady(true);
     }
   });
-  return {user, ready};
+  return {me, ready};
 }
 
 export function useMe(): [DetailedUser | null, UserCallbacks] {
   const [me, setMe] = useRawMe();
   const refetchMe = useCallback(async function (): Promise<void> {
     const url = SERVER_PATH_PREFIX + SERVER_PATHS["fetchUser"];
-    const response = await axios.post(url, {}, {validateStatus: () => true});
-    if (response.status === 200) {
+    const response = await axios.post<ResponseData<"fetchUser">>(url, {}, {validateStatus: () => true});
+    if (response.status === 200 && !("error" in response.data)) {
       const me = response.data;
+      GtagUtil.set("user_properties", [["id", me.id], ["name", me.name], ["screen_name", me.screenName]]);
       setMe(me);
     } else {
       setMe(null);
--- client/util/gtag.ts
+++ client/util/gtag.ts
@@ -4,8 +4,19 @@
 export class GtagUtil {
 
   public static event(name: string, params: Iterable<readonly [string, string]>) {
-    console.log("ga event send", name);
-    gtag("event", name, Object.fromEntries(params));
+    const actualParams = Object.fromEntries(params);
+    console.log("ga event", name, actualParams);
+    if (typeof gtag !== "undefined") {
+      gtag("event", name, actualParams);
+    }
+  }
+
+  public static set(id: string, config: Iterable<readonly [string, string]>) {
+    const actualConfig = Object.fromEntries(config);
+    console.log("ga set", id, actualConfig);
+    if (typeof gtag !== "undefined") {
+      gtag("set", id, actualConfig);
+    }
   }
 
 }
