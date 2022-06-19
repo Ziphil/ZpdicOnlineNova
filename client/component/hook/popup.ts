03fb4c8f6365a62070a306f7e663fcacbe47553b
Ziphil <yfe23457@nifty.com>
2022-06-19 21:13:46 +0900

    リファクタリング

--- client/component/hook/popup.ts
+++ client/component/hook/popup.ts
@@ -18,28 +18,28 @@ export function usePopup(): [Array<PopupSpec>, PopupCallbacks] {
   const clearAllPopups = useCallback((): void => {
     setPopupSpecs([]);
   }, [setPopupSpecs]);
-  const addPopup = useCallback(function (type: string, style: PopupStyle, timeout: number | null): number {
+  const addPopup = useCallback(function (type: string, scheme: PopupScheme, timeout: number | null): number {
     const date = new Date();
     const id = date.getTime();
-    setPopupSpecs((popupSpecs) => [...popupSpecs, {id, type, style}]);
+    setPopupSpecs((popupSpecs) => [...popupSpecs, {id, type, scheme}]);
     if (timeout !== null) {
       setTimeout(() => clearPopup(id), timeout);
     }
     return id;
   }, [setPopupSpecs, clearPopup]);
   const addErrorPopup = useCallback(function (type: string, timeout: number | null = 5000): number {
-    const id = addPopup(type, "error", timeout);
+    const id = addPopup(type, "red", timeout);
     return id;
   }, [addPopup]);
   const addInformationPopup = useCallback(function (type: string, timeout: number | null = 5000): number {
-    const id = addPopup(type, "information", timeout);
+    const id = addPopup(type, "blue", timeout);
     return id;
   }, [addPopup]);
   return [popupSpecs, {addErrorPopup, addInformationPopup, clearPopup, clearAllPopups}];
 }
 
-type PopupStyle = "error" | "information";
-type PopupSpec = {id: number, type: string, style: PopupStyle};
+type PopupScheme = "red" | "blue";
+type PopupSpec = {id: number, type: string, scheme: PopupScheme};
 
 type PopupCallbacks = {
   addErrorPopup: (type: string, timeout?: number | null) => number,
