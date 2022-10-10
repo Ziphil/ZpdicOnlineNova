4453b891aea42b2ecc75a89fb04f46acafb293fc
Ziphil <yfe23457@nifty.com>
2022-10-10 18:41:51 +0900

    変更

--- server/util/log.ts
+++ server/util/log.ts
@@ -5,13 +5,13 @@ export class LogUtil {
 
   public static log(place: string, object: any): void {
     const objectString = (typeof object === "string") ? object : JSON.stringify(object);
-    const message = `![${place}] ${objectString}`;
+    const message = `!<${place}> ${objectString}`;
     console.log(message);
   }
 
   public static error(place: string, object: any, error: Error): void {
     const objectString = (typeof object === "string") ? object : JSON.stringify(object);
-    const message = `![${place}] ${objectString}`;
+    const message = `!!<${place}> ${objectString}`;
     console.error(message);
     console.error(error);
   }
