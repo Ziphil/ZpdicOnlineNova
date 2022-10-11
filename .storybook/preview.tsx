4414318c3dea0713ce34e4b2e4980920232ce332
Ziphil <yfe23457@nifty.com>
2022-10-11 23:32:48 +0900

    Storybookでテーマを確認できるよう変更

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,4 +1,4 @@
-//
+// @ts-nocheck
 
 import {
   BaseDecorators,
@@ -17,6 +17,14 @@ export const parameters = {
   },
   grid: {
     cellSize: 16
+  },
+  themes: {
+    default: "light",
+    list: [
+      {name: "light", class: "theme-light", color: "#F7F2ED"},
+      {name: "dark", class: "theme-dark", color: "#000000"}
+    ],
+    target: "html"
   }
 } as Parameters;
 
