a27a1e7caa1301a340794fc038347f3e415a4922
Ziphil <yfe23457@nifty.com>
2023-12-28 12:42:19 +0900

    トップページのヒーロー部分を作成

---
+++ client-new/component/page/top-page/hero-constant.ts
@@ -0,0 +1,28 @@
+//
+
+
+export const DECORATION_TEXT = `
+  γλῶσσα, Ion. γλάσσα, Herod. 3.84, al., SIG 1002.7 (Milet.), Schwyzer 692 (Chios), Att. γλῶττα, ης, ἡ,
+  tongue, Od. 3.332, etc.
+  b. γ. λάρυγγος, = γλωττίς, larynx, Gal. UP 7.13.
+  2. tongue, as the organ of speech, γλώσσης χάριν through love of talking, Hes. Op. 709, A. Ch. 266; γλώσσῃ ματαίᾳ Id. Pr. 331, cf. Eu. 830; γλώσσης ἀκρατής Id. Pr. 884 (lyr.); μεγάλης γ. κόμποι S. Ant. 128; γλώσσῃ δεινός, θρασύς, Id. OC 806, Aj. 1142; ἡ γ. ὀμώμοχʼ ἡ δὲ φρὴν ἀνώμοτος E. Hipp. 612: with Preps., ἀπὸ γλώσσης by frankness of speech, Thgn. 63; φθέγγεσθαι Pi. O. 6.13 (but ἀπὸ γ. ληίσσεται, opp. χερσὶ βίῃ, of fraud opp. violence, Hes. Op. 322); also, by word of mouth, Hdt. 1.123, Th. 7.10, Arr. An. 2.14.1; τῷ νῷ θʼ ὁμοίως κἀπὸ τῆς γ. λέγω S. OC 936; τὰ γλώσσης ἄπο, i.e. our words, E. Ba. 1049; ἀπὸ γ. φράσω by heart, opp. γράμμασιν, Cratin. 122; οὐκ ἀπὸ γλώσσης not from mere word of mouth, but after full argument, A. Ag. 813; μὴ διὰ γλώσσης without using the tongue, E. Supp. 112; ἐν ὄμμασιν . . δεδορκὼς κοὐ κατὰ γλῶσσαν κλύων S. Tr. 747:—phrases: πᾶσαν γλῶτταν βασάνιζε try every art of tongue, Ar. V. 547; πᾶσαν ἱέναι γλῶσσαν let loose oneʼs whole tongue, speak withoutrestraint, S. El. 596; πολλὴν γ. ἐγχέας μάτην Id. Fr. 929; κακὰ γ. slander, Pi. P. 4.283: pl., ἐν κερτομίοις γλώσσαις, i.e. with blasphemies, S. Ant. 962 (lyr.), cf. Aj. 199 (lyr.): βοῦς, κ ῇς ἐπὶ γλώσσῃ, v. βοῦς, κλείς.
+  3. of persons, one who is all tongue, speaker, of Pericles, μεγίστη γ. τῶν Ἑλληνίδων Cratin. 293, cf. Ar. Fr. 629 (s. v. l.).
+  4. ἡ γ. τοῦ ταμιείου the advocacy of the fiscus, Philostr. VS 2.29.
+  II. language, ἄλλη δʼ ἄλλων γ. μεμιγμένη Od. 19.175, cf. Il. 2.804; γλῶσσαν ἱέναι speak a language or dialect, Hdt. 1.57; γ. Ἑλληνίδα, Δωρίδα ἱέναι, Id. 9.16, Th. 3.112, cf. A. Pers. 406, Ch. 564; γλῶσσαν νομίζειν Hdt. 1.142, 4.183; γλώσσῃ χρῆσθαι Id. 4.109; κατὰ τὴν ἀρχαίαν γ. Arist. Rh. 1357b10; dialect, ἡ Ἀττικὴ γ. Demetr. Eloc. 177; but also Δωρὶς διάλεκτος μία ὑφʼ ἥν εἰσι γ. πολλαί Tryph. ap. Sch.D.T. p.320 H.
+  2. obsolete or foreign word, which needs explanation, Arist. Rh. 1410b12, Po. 1457b4, Plu. 2.406f: hence Γλῶσσαι, title of works by Philemon and others.
+  3. people speaking a distinct language, LXX Ju. 3.8 (pl.), interpol. in Scyl. 15.
+  III. anything shaped like the tongue (cf. γλῶσσαι ὡσεὶ πυρός Act.Ap. 2.3).
+  1. in Music, reed or tongue of a pipe, Aeschin. 3.229, Arist. HA 565a24, Thphr. HP 4.11.4, etc.
+  2. tongue or thong of leather, shoe-latchet, Pl.Com. 51, Aeschin.Socr. 57.
+  3. tongue of land, App. Pun. 121, cf. 95.
+  4. ingot, γ. χρυσῆ LXX Jo. 7.21.
+  5. marking on the liver, in divination, Hsch. (γλῶσσα from γλωχ-yǎ, cf. γλώξ, γλωχίς; γλάσσα from *γλᾰχ-yǎ, weak grade of same root.)
+
+  βύβλος and βίβλος (v. sub fin.), ἡ,
+  the Egyptian papyrus, Cyperus Papyrus, Hdt. 2.92, A. Supp. 761, Str. 17.1.15: in pl., stalks of papyrus, PTeb. 308.7 (ii A. D.).
+  2. rind enclosing the pith of this plant, Thphr. HP 4.8.4, etc.: generally, bark, φελλῶν καὶ βύβλων Pl. Plt. 288e, cf. Hdt. 2.96, Plot. 2.7.2.
+  b. in pl., slices of the pith used as writing-material, Hdt. 5.58, Hermipp. 63.13: sg., strip of β., βύβλον εὐρύναντες ἀντὶ διαδήματος Ph. 2.522.
+  3. roll of papyrus, book, Hdt. 2.100, A. Supp. 947, etc.: heterocl. pl., βύβλα, τά, AP 9.98 (Stat. Flacc.); esp. of sacred or magical writings, βίβλων ὅμαδον Μουσαίου καὶ Ὀρφέως Pl. R. 364e, cf. D. 18.259, Act.Ap. 19.19, PPar. 19.1 (ii A. D.); ἱεραί β. OGI 56.70 (Canopus, iii B. C.); β. ἱερατική PTeb. 291.43 (ii A. D.); so of the Scriptures, ἡ β. γενέσεως οὐρανοῦ καὶ γῆς LXX Ge. 2.4, etc.; ἡ β. the Sacred Writings, Aristeas 316; β. Μωυσέως, ψαλμῶν, προφητῶν, Ev.Marc. 12.26, Act.Ap. 1.20, 7.42; β. ζωῆς Ep.Phil. 4.3: pl., of magical books, Act. Ap. 19.19.
+  4. a division of a book, Plb. 4.87.12, D.S. 1.4, etc.; αἱ β. the nine books of Hdt., Luc. Herod. 1.
+  II. β. στεφανωτρίς flowering head of papyrus, Theopomp. Hist. 22c, Plu. Ages. 36. [ῠ, A. Supp. 761.] (βύβλος, βύβλινος, βυβλίον, etc., are the original forms: βιβλ- seems to have arisen in Attic by assimilation in βιβλίον, and is found in earlier Attic Inscrr., cf. IG 2.1b, etc., and prevails in Ptolemaic papyri; Inscrr. vary, βυβλία Test.Epict. 8.32 (iii/ii B. C.); βιβλία IG 5(1).1390.12 (Andania, i B. C.); in Roman times βυβλ- was restored.)
+`;
--- client-new/component/page/top-page/hero.tsx
+++ client-new/component/page/top-page/hero.tsx
@@ -1,9 +1,11 @@
 //
 
+import {faBook, faSignIn} from "@fortawesome/sharp-regular-svg-icons";
 import {ReactElement} from "react";
-import {AdditionalProps, useTrans} from "zographia";
+import {AdditionalProps, GeneralIcon, Link, LinkIconbag, aria, useTrans} from "zographia";
 import {Logo} from "/client-new/component/atom/logo";
 import {create} from "/client-new/component/create";
+import {DECORATION_TEXT} from "./hero-constant";
 
 
 export const Hero = create(
@@ -22,6 +24,19 @@ export const Hero = create(
         <p styleName="catch">
           {trans("catch")}
         </p>
+        <div styleName="button-group">
+          <Link styleName="button" variant="light">
+            <LinkIconbag><GeneralIcon icon={faSignIn}/></LinkIconbag>
+            {trans("login")}
+          </Link>
+          <Link styleName="link">
+            <LinkIconbag><GeneralIcon icon={faBook}/></LinkIconbag>
+            {trans("dictionary")}
+          </Link>
+        </div>
+        <div styleName="decoration" {...aria({hidden: true})}>
+          {DECORATION_TEXT}
+        </div>
       </div>
     );
 
