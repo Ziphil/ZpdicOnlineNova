9d7f1206c4d216dfcc9146371f5718e788a219bf
Ziphil <yfe23457@nifty.com>
2023-12-31 09:54:58 +0900

    変更

--- client-new/component/compound/header/header.tsx
+++ client-new/component/compound/header/header.tsx
@@ -5,6 +5,7 @@ import {Link as RouterLink} from "react-router-dom";
 import {AdditionalProps} from "zographia";
 import {Logo} from "/client-new/component/atom/logo";
 import {create} from "/client-new/component/create";
+import {VERSION} from "/client-new/variable";
 
 
 export const Header = create(
@@ -18,9 +19,12 @@ export const Header = create(
     const node = (
       <header styleName="root" {...rest}>
         <div styleName="left">
-          <RouterLink to="/">
-            <Logo styleName="logo"/>
+          <RouterLink styleName="logo" to="/">
+            <Logo/>
           </RouterLink>
+          <div styleName="version">
+            {VERSION}
+          </div>
         </div>
         <div styleName="right">
         </div>
--- client-new/component/core/routing/routing.tsx
+++ client-new/component/core/routing/routing.tsx
@@ -12,7 +12,7 @@ const router = createBrowserRouter([
 
 
 export const Routing = create(
-  require("./routing.scss"), "Routing",
+  null, "Routing",
   function ({
   }: {
   }): ReactElement {
