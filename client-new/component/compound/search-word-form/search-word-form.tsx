1cdf91df79909800adbc188233f4d4ddba9a18fe
Ziphil <yfe23457@nifty.com>
2024-01-12 17:29:47 +0900

    変更

--- client-new/component/compound/search-word-form/search-word-form.tsx
+++ client-new/component/compound/search-word-form/search-word-form.tsx
@@ -1,13 +1,27 @@
 //
 
-import {faSearch} from "@fortawesome/sharp-regular-svg-icons";
+import {faSearch, faShuffle} from "@fortawesome/sharp-regular-svg-icons";
 import merge from "lodash-es/merge";
 import {Dispatch, ReactElement, SetStateAction, useCallback} from "react";
-import {AdditionalProps, CheckableContainer, CheckableLabel, Checkbox, GeneralIcon, Input, InputAddon, Radio, RadioGroup, useTrans} from "zographia";
+import {DeepPartial} from "ts-essentials";
+import {
+  AdditionalProps,
+  Button,
+  ButtonIconbag,
+  CheckableContainer,
+  CheckableLabel,
+  Checkbox,
+  GeneralIcon,
+  Input,
+  InputAddon,
+  Radio,
+  RadioGroup,
+  useTrans
+} from "zographia";
 import {OrderDirection, OrderDirectionSelect} from "/client-new/component/compound/order-direction-select";
 import {OrderModeSelect} from "/client-new/component/compound/order-mode-select";
 import {create} from "/client-new/component/create";
-import {WORD_ORDER_MODES, WordOrderMode, WordParameter} from "/client-new/skeleton";
+import {NormalWordParameter, WordModeUtil, WordOrderMode, WordParameter, WordTypeUtil} from "/client-new/skeleton";
 import {preventDefault} from "/client-new/util/form";
 
 
@@ -27,39 +41,78 @@ export const SearchWordForm = create(
 
     const actualParameter = WordParameter.toNormal(parameter);
 
-    const handleOrderModeSet = useCallback(function (orderMode: WordOrderMode): void {
-      onParameterSet?.((prevParameter) => merge({}, prevParameter, {order: {mode: orderMode}}));
+    const handleSet = useCallback(function (parameter: DeepPartial<NormalWordParameter>): void {
+      onParameterSet?.((prevParameter) => {
+        const prevNormalParameter = WordParameter.toNormal(prevParameter);
+        if (parameter.options?.shuffleSeed !== undefined) {
+          const nextParameter = merge({}, prevNormalParameter, parameter);
+          return nextParameter;
+        } else {
+          const nextParameter = merge({}, prevNormalParameter, parameter, {options: {shuffleSeed: null}});
+          return nextParameter;
+        }
+      });
     }, [onParameterSet]);
 
+    const handleTextSet = useCallback(function (text: string): void {
+      handleSet({text});
+    }, [handleSet]);
+
+    const handleModeSet = useCallback(function (mode: string): void {
+      handleSet({mode: WordModeUtil.cast(mode)});
+    }, [handleSet]);
+
+    const handleTypeSet = useCallback(function (type: string): void {
+      handleSet({type: WordTypeUtil.cast(type)});
+    }, [handleSet]);
+
+    const handleIgnoreCaseSet = useCallback(function (ignoreCase: boolean): void {
+      handleSet({options: {ignore: {case: ignoreCase}}});
+    }, [handleSet]);
+
+    const handleEnableSuggestionsSet = useCallback(function (enableSuggestions: boolean): void {
+      handleSet({options: {enableSuggestions}});
+    }, [handleSet]);
+
+    const handleOrderModeSet = useCallback(function (orderMode: WordOrderMode): void {
+      handleSet({order: {mode: orderMode}});
+    }, [handleSet]);
+
     const handleOrderDirectionSet = useCallback(function (orderDirection: OrderDirection): void {
-      onParameterSet?.((prevParameter) => merge({}, prevParameter, {order: {direction: orderDirection}}));
-    }, [onParameterSet]);
+      handleSet({order: {direction: orderDirection}});
+    }, [handleSet]);
+
+    const shuffleResult = useCallback(function (): void {
+      handleSet({options: {shuffleSeed: Date.now().toString()}});
+    }, [handleSet]);
 
     const node = (
       <form styleName="root" onSubmit={preventDefault} {...rest}>
         <Input
           styleName="input"
           type="search"
+          value={actualParameter.text}
+          onSet={handleTextSet}
         >
           <InputAddon position="left">
             <GeneralIcon styleName="icon" icon={faSearch}/>
           </InputAddon>
         </Input>
         <div styleName="radio-group">
-          <RadioGroup>
+          <RadioGroup name="mode" value={actualParameter.mode} onSet={handleModeSet}>
             {FORM_WORD_MODES.map((mode) => (
               <CheckableContainer key={mode}>
-                <Radio/>
+                <Radio value={mode}/>
                 <CheckableLabel>{trans(`mode.${mode}`)}</CheckableLabel>
               </CheckableContainer>
             ))}
           </RadioGroup>
         </div>
         <div styleName="radio-group">
-          <RadioGroup>
+          <RadioGroup name="type" value={actualParameter.type} onSet={handleTypeSet}>
             {FORM_WORD_TYPES.map((type) => (
               <CheckableContainer key={type}>
-                <Radio/>
+                <Radio value={type}/>
                 <CheckableLabel>{trans(`type.${type}`)}</CheckableLabel>
               </CheckableContainer>
             ))}
@@ -67,17 +120,17 @@ export const SearchWordForm = create(
         </div>
         <div styleName="radio-group">
           <CheckableContainer>
-            <Checkbox/>
+            <Checkbox checked={actualParameter.options.ignore.case} onSet={handleIgnoreCaseSet}/>
             <CheckableLabel>{trans("ignoreCase")}</CheckableLabel>
           </CheckableContainer>
           <CheckableContainer>
-            <Checkbox/>
+            <Checkbox checked={actualParameter.options.enableSuggestions} onSet={handleEnableSuggestionsSet}/>
             <CheckableLabel>{trans("enableSuggestions")}</CheckableLabel>
           </CheckableContainer>
         </div>
         <div styleName="row">
           <OrderModeSelect
-            orderModeOptions={WORD_ORDER_MODES}
+            orderModeOptions={FORM_WORD_ORDER_MODES}
             orderMode={actualParameter.order.mode}
             onSet={handleOrderModeSet}
           />
@@ -85,6 +138,12 @@ export const SearchWordForm = create(
             orderDirection={actualParameter.order.direction}
             onSet={handleOrderDirectionSet}
           />
+        </div>
+        <div styleName="row">
+          <Button variant="text" scheme="secondary" onClick={shuffleResult}>
+            <ButtonIconbag><GeneralIcon icon={faShuffle}/></ButtonIconbag>
+            {trans("shuffleResult")}
+          </Button>
         </div>
       </form>
     );
@@ -96,3 +155,4 @@ export const SearchWordForm = create(
 
 const FORM_WORD_MODES = ["both", "name", "equivalent", "tag", "content"] as const;
 const FORM_WORD_TYPES = ["prefix", "part", "exact", "regular"] as const;
+const FORM_WORD_ORDER_MODES = ["unicode", "updatedDate", "createdDate"] as const;
