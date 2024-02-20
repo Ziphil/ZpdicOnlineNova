f29e9cd2f9d2db42f2f688abdb8423a3c58f8097
Ziphil <yfe23457@nifty.com>
2024-02-20 23:06:55 +0900

    変更

--- client-new/component/page/dictionary-setting-file-part/dictionary-setting-file-part.tsx
+++ client-new/component/page/dictionary-setting-file-part/dictionary-setting-file-part.tsx
@@ -2,8 +2,9 @@
 
 import {ReactElement} from "react";
 import {useOutletContext} from "react-router-dom";
-import {AdditionalProps, useTrans} from "zographia";
+import {AdditionalProps, MultiLineText, useTrans} from "zographia";
 import {EnhancedDictionary} from "/client/skeleton/dictionary";
+import {Link} from "/client-new/component/atom/link";
 import {create} from "/client-new/component/create";
 import {DownloadDictionaryButton} from "/client-new/component/form/download-dictionary-button";
 import {UploadDictionaryButton} from "/client-new/component/form/upload-dictionary-button";
@@ -25,10 +26,18 @@ export const DictionarySettingFilePart = create(
       <div styleName="root" {...rest}>
         <section styleName="section">
           <h3 styleName="heading">{trans("heading.upload")}</h3>
+          <MultiLineText styleName="description">
+            {trans("description.upload")}
+          </MultiLineText>
           <UploadDictionaryButton dictionary={dictionary}/>
         </section>
         <section styleName="section">
           <h3 styleName="heading">{trans("heading.download")}</h3>
+          <MultiLineText styleName="description">
+            {transNode("description.download", {
+              link: (parts) => <Link href="/document" variant="unstyledUnderline">{parts}</Link>
+            })}
+          </MultiLineText>
           <DownloadDictionaryButton dictionary={dictionary}/>
         </section>
       </div>
