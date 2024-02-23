aa333d3a5e7fae016fac038836145b23e4e3ccce
Ziphil <yfe23457@nifty.com>
2024-02-23 22:47:50 +0900

    バージョン番号を変更

--- client-new/variable.ts
+++ client-new/variable.ts
@@ -1,7 +1,7 @@
 //
 
 
-export const VERSION = "3.0.0-alpha.4" || "?";
+export const VERSION = "3.0.0-alpha.5" || "?";
 export const RECAPTCHA_KEY = process.env["RECAPTCHA_KEY"] || "dummy";
 export const AWS_STORAGE_BUCKET = process.env["AWS_STORAGE_BUCKET"] || "dummy";
 export const ANALYTICS_ID = process.env["ANALYTICS_ID"] || "";
