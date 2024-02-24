f6ea35aa2b2b9f9da015659de4f1aa0fd71803c9
Ziphil <yfe23457@nifty.com>
2024-02-25 01:11:37 +0900

    変更

--- client-new/component/core/routing/toplevel-route.tsx
+++ client-new/component/core/routing/toplevel-route.tsx
@@ -2,6 +2,7 @@
 
 import {ReactElement, Suspense, useEffect} from "react";
 import {Outlet, useLocation} from "react-router-dom";
+import {useMount} from "react-use";
 import {create} from "/client-new/component/create";
 import {LoadingPage} from "/client-new/component/page/loading-page";
 import {sendAnalyticsEvent} from "/client-new/util/gtag";
@@ -18,6 +19,13 @@ export const ToplevelRoute = create(
     useEffect(() => {
       sendAnalyticsEvent("page_view", [["page_path", location.pathname]]);
     }, [location.pathname]);
+
+    useMount(() => {
+      const anyWindow = window as any;
+      if (anyWindow) {
+        (anyWindow.adsbygoogle = anyWindow.adsbygoogle || []).push({});
+      }
+    });
 
     return (
       <Suspense fallback={<LoadingPage/>}>
