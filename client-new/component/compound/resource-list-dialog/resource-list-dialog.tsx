8079a4e9ff6272a57d0e72a474269ef04e151bd0
Ziphil <yfe23457@nifty.com>
2024-03-02 10:27:31 +0900

    変更

--- client-new/component/compound/resource-list-dialog/resource-list-dialog.tsx
+++ client-new/component/compound/resource-list-dialog/resource-list-dialog.tsx
@@ -1,17 +1,26 @@
 //
 
+import {faCheck, faInfoCircle} from "@fortawesome/sharp-regular-svg-icons";
 import {Fragment, ReactElement, cloneElement, useCallback, useState} from "react";
 import {
+  Button,
+  ButtonIconbag,
+  Callout,
+  CalloutBody,
+  CalloutIconContainer,
   Dialog,
   DialogBody,
   DialogCloseButton,
   DialogPane,
+  GeneralIcon,
+  MultiLineText,
   useTrans
 } from "zographia";
 import {ResourceList} from "/client-new/component/compound/resource-list/resource-list";
 import {create} from "/client-new/component/create";
-import {useResponse} from "/client-new/hook/request";
+import {invalidateResponses, useRequest, useResponse} from "/client-new/hook/request";
 import {EnhancedDictionary} from "/client-new/skeleton";
+import {switchResponse} from "/client-new/util/response";
 
 
 export const ResourceListDialog = create(
@@ -26,16 +35,26 @@ export const ResourceListDialog = create(
     className?: string
   }): ReactElement {
 
-    const {trans} = useTrans("editWordDialog");
+    const {trans} = useTrans("resourceListDialog");
 
     const number = dictionary.number;
     const [[resources] = []] = useResponse("fetchResources", {number}, {keepPreviousData: true});
+
+    const request = useRequest();
 
     const [open, setOpen] = useState(false);
 
     const openDialog = useCallback(function (): void {
       setOpen(true);
     }, []);
+
+    const enableMarkdown = useCallback(async function (): Promise<void> {
+      const number = dictionary.number;
+      const response = await request("changeDictionarySettings", {number, settings: {enableMarkdown: true}});
+      await switchResponse(response, async () => {
+        await invalidateResponses("fetchDictionary", (data) => data.number === dictionary.number || data.paramName === dictionary.paramName);
+      });
+    }, [dictionary.number, dictionary.paramName, request]);
 
     return (
       <Fragment>
@@ -44,6 +63,24 @@ export const ResourceListDialog = create(
           <DialogPane styleName="pane">
             <DialogCloseButton/>
             <DialogBody>
+              {(dictionary.settings.enableMarkdown) ? (
+                <MultiLineText styleName="message" is="p">
+                  {trans("message")}
+                </MultiLineText>
+              ) : (
+                <Callout styleName="callout">
+                  <CalloutIconContainer><GeneralIcon icon={faInfoCircle}/></CalloutIconContainer>
+                  <CalloutBody styleName="callout-body">
+                    <MultiLineText is="p">
+                      {trans("callout")}
+                    </MultiLineText>
+                    <Button styleName="callout-button" variant="light" onClick={enableMarkdown}>
+                      <ButtonIconbag><GeneralIcon icon={faCheck}/></ButtonIconbag>
+                      {trans("button.enable")}
+                    </Button>
+                  </CalloutBody>
+                </Callout>
+              )}
               <ResourceList dictionary={dictionary} resources={resources} pageSpec={{size: 40}} showCode={true}/>
             </DialogBody>
           </DialogPane>
