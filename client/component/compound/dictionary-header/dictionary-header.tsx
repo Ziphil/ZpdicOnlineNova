ad91465f78e632ab6544eb62a7e6b7c96088b9eb
Ziphil <yfe23457@nifty.com>
2024-04-02 22:09:45 +0900

    リファクタリング

--- client/component/compound/dictionary-header/dictionary-header.tsx
+++ client/component/compound/dictionary-header/dictionary-header.tsx
@@ -1,31 +1,14 @@
 /* eslint-disable react/jsx-closing-bracket-location */
 
-import {faBook, faCircleInfo, faCog, faCommentQuestion, faImage, faListCheck, faQuotes} from "@fortawesome/sharp-regular-svg-icons";
+import {faBook, faCircleInfo, faCog, faImage, faListCheck, faQuotes} from "@fortawesome/sharp-regular-svg-icons";
 import {ReactElement} from "react";
-import {
-  AdditionalProps,
-  Button,
-  ButtonIconbag,
-  GeneralIcon,
-  Indicator,
-  MultiLineText,
-  SingleLineText,
-  TabIconbag,
-  TabList,
-  useTrans
-} from "zographia";
-import {fakNoteCirclePlus, fakQuotesCirclePlus} from "/client/component/atom/icon";
-import {Link} from "/client/component/atom/link";
+import {AdditionalProps, GeneralIcon, Indicator, TabIconbag, TabList, useTrans} from "zographia";
 import {LinkTab} from "/client/component/atom/tab";
-import {UserAvatar} from "/client/component/atom/user-avatar";
-import {AddCommissionDialog} from "/client/component/compound/add-commission-dialog";
-import {EditExampleDialog} from "/client/component/compound/edit-example-dialog";
-import {EditWordDialog} from "/client/component/compound/edit-word-dialog";
 import {MainContainer} from "/client/component/compound/page";
 import {create} from "/client/component/create";
 import {useResponse, useSuspenseResponse} from "/client/hook/request";
 import {EnhancedDictionary} from "/client/skeleton";
-import {DictionaryHeaderStatusView} from "./dictionary-header-status-view";
+import {DictionaryHeaderTop} from "./dictionary-header-top";
 
 
 export const DictionaryHeader = create(
@@ -42,13 +25,10 @@ export const DictionaryHeader = create(
     className?: string
   } & AdditionalProps): ReactElement {
 
-    const {trans, transNode} = useTrans("dictionaryHeader");
+    const {trans} = useTrans("dictionaryHeader");
 
     const [canEdit] = useSuspenseResponse("fetchDictionaryAuthorization", {identifier: dictionary.number, authority: "edit"});
     const [canOwn] = useSuspenseResponse("fetchDictionaryAuthorization", {identifier: dictionary.number, authority: "own"});
-    const [authorizedUsers] = useResponse("fetchDictionaryAuthorizedUsers", {number: dictionary.number, authority: "editOnly"});
-
-    const [sizes] = useResponse("fetchDictionarySizes", {number: dictionary.number});
 
     const [[offers] = []] = useResponse("fetchExampleOffers", (canEdit) && {size: 1, offset: 0});
     const [[offerExamples] = []] = useResponse("fetchExamplesByOffer", (canEdit && offers !== undefined && offers.length > 0) && {number: dictionary.number, offerId: offers[0].id, size: 1, offset: 0});
@@ -57,62 +37,7 @@ export const DictionaryHeader = create(
     return (
       <header styleName="root" {...rest}>
         <MainContainer styleName="container" width={width}>
-          <div styleName="top">
-            <div styleName="top-left">
-              <div>
-                <MultiLineText styleName="name" is="h2" maxLineCount={2} lineHeight="narrowest">
-                  {dictionary.name}
-                </MultiLineText>
-                <div styleName="user-container">
-                  <span styleName="user">
-                    <UserAvatar styleName="avatar" user={dictionary.user}/>
-                    <SingleLineText is="span">
-                      <Link href={`/user/${dictionary.user.name}`} variant="unstyledSimple">
-                        {dictionary.user.screenName}
-                      </Link>
-                    </SingleLineText>
-                  </span>
-                  {(authorizedUsers !== undefined && authorizedUsers.length > 0) && (
-                    <span styleName="user-count">
-                      {transNode("userCount", {
-                        count: authorizedUsers.length,
-                        plus: (parts) => <span styleName="plus">{parts}</span>
-                      })}
-                    </span>
-                  )}
-                </div>
-              </div>
-              <div styleName="operation">
-                {(canEdit) && (
-                  <div styleName="operation-row">
-                    <EditWordDialog dictionary={dictionary} initialData={null} trigger={(
-                      <Button variant="light" {...rest}>
-                        <ButtonIconbag><GeneralIcon icon={fakNoteCirclePlus}/></ButtonIconbag>
-                        {trans("button.addWord")}
-                      </Button>
-                    )}/>
-                    <EditExampleDialog dictionary={dictionary} initialData={null} trigger={(
-                      <Button variant="light" {...rest}>
-                        <ButtonIconbag><GeneralIcon icon={fakQuotesCirclePlus}/></ButtonIconbag>
-                        {trans("button.addExample")}
-                      </Button>
-                    )}/>
-                  </div>
-                )}
-                <div styleName="operation-row">
-                  <AddCommissionDialog dictionary={dictionary} trigger={(
-                    <Button scheme="secondary" variant="underline" {...rest}>
-                      <ButtonIconbag><GeneralIcon icon={faCommentQuestion}/></ButtonIconbag>
-                      {trans("button.addCommission")}
-                    </Button>
-                  )}/>
-                </div>
-              </div>
-            </div>
-            <div styleName="top-right">
-              <DictionaryHeaderStatusView dictionary={dictionary} sizes={sizes}/>
-            </div>
-          </div>
+          <DictionaryHeaderTop dictionary={dictionary}/>
           <TabList styleName="tab-list" value={tabValue ?? ""}>
             <LinkTab value="dictionary" href={`/dictionary/${dictionary.number}`}>
               <TabIconbag><GeneralIcon icon={faBook}/></TabIconbag>
