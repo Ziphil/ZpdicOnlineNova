17832845feb357d52eb6d37163a38a3c5aa5bbc8
Ziphil <yfe23457@nifty.com>
2024-04-07 17:19:48 +0900

    変更

--- client/skeleton/example-offer-parameter/normal-example-offer-parameter.ts
+++ client/skeleton/example-offer-parameter/normal-example-offer-parameter.ts
@@ -1,5 +1,8 @@
 //
 
+import merge from "lodash.merge";
+import {Search} from "/client/hook/search";
+
 
 export interface NormalExampleOfferParameter {
 
@@ -18,6 +21,20 @@ export namespace NormalExampleOfferParameter {
   export const DAILY = {
     kind: "normal",
     catalog: "zpdicDaily"
-  }satisfies NormalExampleOfferParameter;
+  } satisfies NormalExampleOfferParameter;
+
+  export function deserialize(search: Search): NormalExampleOfferParameter {
+    const catalog = (search.has("catalog")) ? search.get("catalog") : undefined;
+    const parameter = merge({}, DAILY, {catalog});
+    return parameter;
+  }
+
+  export function serialize(parameter: NormalExampleOfferParameter): Search {
+    const search = new URLSearchParams();
+    if (parameter.catalog !== null) {
+      search.set("catalog", parameter.catalog);
+    }
+    return search;
+  }
 
 }
