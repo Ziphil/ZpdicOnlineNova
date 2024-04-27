ea8203709ad36e6751a1e2f4af14271b1d9f8e36
Ziphil <yfe23457@nifty.com>
2024-04-27 18:44:53 +0900

    関連語のサジェストで訳語検索もするよう変更

--- client/component/atom/relation-word-select/relation-word-select.tsx
+++ client/component/atom/relation-word-select/relation-word-select.tsx
@@ -25,7 +25,7 @@ export const RelationWordSelect = create(
 
     const loadOptions = useCallback(async function (pattern: string): Promise<Array<Word>> {
       const number = dictionary.number;
-      const parameter = {...NormalWordParameter.EMPTY, text: pattern, mode: "name" as const};
+      const parameter = {...NormalWordParameter.EMPTY, text: pattern, mode: "both", type: "part"} as NormalWordParameter;
       const response = await request("searchWord", {number, parameter, offset: 0, size: 20}, {ignoreError: true});
       return switchResponse(response, ({words: [hitWords]}) => {
         return hitWords;
