7fd1897b2c7eb0016b431e8b25d2f76101196761
Ziphil <yfe23457@nifty.com>
2024-04-29 18:30:59 +0900

    変更

--- client/util/gtag.ts
+++ client/util/gtag.ts
@@ -2,17 +2,23 @@
 
 
 export function sendAnalyticsEvent(name: string, params: Iterable<readonly [string, string]>): void {
-  const actualParams = Object.fromEntries(params);
-  console.log("ga event", name, actualParams);
-  if (typeof gtag !== "undefined") {
-    gtag("event", name, actualParams);
+  const debug = location.hostname === "localhost";
+  if (!debug) {
+    const actualParams = Object.fromEntries(params);
+    console.log("ga event", name, actualParams);
+    if (typeof gtag !== "undefined") {
+      gtag("event", name, actualParams);
+    }
   }
 }
 
 export function setAnalyticsProperties(id: string, config: Iterable<readonly [string, string]>): void {
-  const actualConfig = Object.fromEntries(config);
-  console.log("ga set", id, actualConfig);
-  if (typeof gtag !== "undefined") {
-    gtag("set", id, actualConfig);
+  const debug = location.hostname === "localhost";
+  if (!debug) {
+    const actualConfig = Object.fromEntries(config);
+    console.log("ga set", id, actualConfig);
+    if (typeof gtag !== "undefined") {
+      gtag("set", id, actualConfig);
+    }
   }
 }
