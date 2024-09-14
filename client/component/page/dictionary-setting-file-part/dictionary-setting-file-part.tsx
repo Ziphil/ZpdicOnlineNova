59451b2414498d420b02ddaf8bbcda7e3c049c0e
Ziphil <yfe23457@nifty.com>
2024-09-14 12:56:26 +0900

    変更

--- client/component/page/dictionary-setting-file-part/dictionary-setting-file-part.tsx
+++ client/component/page/dictionary-setting-file-part/dictionary-setting-file-part.tsx
@@ -1,9 +1,8 @@
 //
 
-import {faInfoCircle} from "@fortawesome/sharp-regular-svg-icons";
 import {ReactElement} from "react";
 import {useOutletContext} from "react-router-dom";
-import {AdditionalProps, Callout, CalloutBody, CalloutIconContainer, GeneralIcon, MultiLineText, useTrans} from "zographia";
+import {AdditionalProps, MultiLineText, useTrans} from "zographia";
 import {Link} from "/client/component/atom/link";
 import {create} from "/client/component/create";
 import {DownloadDictionaryButton} from "/client/component/form/download-dictionary-button";
@@ -39,14 +38,6 @@ export const DictionarySettingFilePart = create(
               link: (parts) => <Link href="/document/other/json-spec" variant="unstyledUnderline">{parts}</Link>
             })}
           </MultiLineText>
-          <Callout styleName="callout">
-            <CalloutIconContainer><GeneralIcon icon={faInfoCircle}/></CalloutIconContainer>
-            <CalloutBody>
-              <MultiLineText is="p">
-                {trans("callout.download")}
-              </MultiLineText>
-            </CalloutBody>
-          </Callout>
           <DownloadDictionaryButton dictionary={dictionary}/>
         </section>
       </div>
