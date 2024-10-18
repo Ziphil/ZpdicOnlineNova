df569d33b9faceb2432c1d7d64084daf9673ee8c
Ziphil <yfe23457@nifty.com>
2024-10-19 01:16:24 +0900

    例文集を追加

--- client/component/compound/search-example-offer-form/search-example-offer-form.tsx
+++ client/component/compound/search-example-offer-form/search-example-offer-form.tsx
@@ -43,4 +43,4 @@ export const SearchExampleOfferForm = create(
 );
 
 
-const CATALOGS = ["zpdicDaily", "appleAlpha", "appleBeta", "appleGamma", "survival"];
+const CATALOGS = ["zpdicDaily", "appleAlpha", "appleBeta", "appleGamma", "survival", "weaving"];
