538d5b53e34ae0e1b87e6e88370c2775e5570276
Ziphil <yfe23457@nifty.com>
2025-03-01 16:57:26 +0900

    変更

--- client/component/compound/article-list/article-card.tsx
+++ client/component/compound/article-list/article-card.tsx
@@ -1,8 +1,10 @@
 /* eslint-disable react/jsx-closing-bracket-location */
 
-import {faEdit, faHashtag, faTrashAlt} from "@fortawesome/sharp-regular-svg-icons";
+import {faEdit, faHashtag, faRight, faTrashAlt} from "@fortawesome/sharp-regular-svg-icons";
 import {ReactElement} from "react";
-import {AdditionalProps, Button, ButtonIconbag, Card, CardBody, CardFooter, GeneralIcon, MultiLineText, Tag, useTrans} from "zographia";
+import {useMatch} from "react-router-dom";
+import {AdditionalProps, Button, ButtonIconbag, Card, CardBody, CardFooter, GeneralIcon, LinkIconbag, MultiLineText, Tag, useTrans} from "zographia";
+import {Link} from "/client/component/atom/link";
 import {Markdown} from "/client/component/atom/markdown";
 import {EditArticleDialog} from "/client/component/compound/edit-article-dialog";
 import {create} from "/client/component/create";
@@ -17,10 +19,12 @@ export const ArticleCard = create(
   function ({
     dictionary,
     article,
+    listed = true,
     ...rest
   }: {
     dictionary: Dictionary,
     article: Article,
+    listed?: boolean,
     className?: string
   } & AdditionalProps): ReactElement {
 
@@ -28,6 +32,7 @@ export const ArticleCard = create(
 
     const [canEdit] = useResponse("fetchDictionaryAuthorization", {identifier: dictionary.number, authority: "edit"});
 
+    const match = useMatch("/dictionary/:identifier/:tabPath?/:subTabPath?");
     const debug = location.hostname === "localhost";
 
     const discardArticle = useDiscardArticle(dictionary, article);
@@ -55,18 +60,28 @@ export const ArticleCard = create(
             {article.content}
           </Markdown>
         </CardBody>
-        {(canEdit) && (
+        {(listed || canEdit) && (
           <CardFooter styleName="footer">
-            <EditArticleDialog dictionary={dictionary} initialData={{type: "article", article}} trigger={(
-              <Button scheme="secondary" variant="underline">
-                <ButtonIconbag><GeneralIcon icon={faEdit}/></ButtonIconbag>
-                {trans("button.edit")}
+            {(listed) && (
+              <Link href={`/dictionary/${match?.params.identifier}/articles/${article.number}`} scheme="secondary" variant="underline">
+                <LinkIconbag><GeneralIcon icon={faRight}/></LinkIconbag>
+                {trans("button.read")}
+              </Link>
+            )}
+            {(canEdit) && (
+              <EditArticleDialog dictionary={dictionary} initialData={{type: "article", article}} trigger={(
+                <Button scheme="secondary" variant="underline">
+                  <ButtonIconbag><GeneralIcon icon={faEdit}/></ButtonIconbag>
+                  {trans("button.edit")}
+                </Button>
+              )}/>
+            )}
+            {(canEdit) && (
+              <Button scheme="red" variant="underline" onClick={discardArticle}>
+                <ButtonIconbag><GeneralIcon icon={faTrashAlt}/></ButtonIconbag>
+                {trans("button.discard")}
               </Button>
-            )}/>
-            <Button scheme="red" variant="underline" onClick={discardArticle}>
-              <ButtonIconbag><GeneralIcon icon={faTrashAlt}/></ButtonIconbag>
-              {trans("button.discard")}
-            </Button>
+            )}
           </CardFooter>
         )}
       </Card>
