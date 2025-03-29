167d2df3074a5b3a217dda30edbfaacb8b7a4d11
Ziphil <yfe23457@nifty.com>
2025-03-30 01:39:53 +0900

    変更

--- client/component/page/user-developer-part/user-developer-part.tsx
+++ client/component/page/user-developer-part/user-developer-part.tsx
@@ -1,8 +1,10 @@
 //
 
+import {faInfoCircle} from "@fortawesome/sharp-regular-svg-icons";
 import {ReactElement} from "react";
 import {useParams} from "react-router-dom";
-import {AdditionalProps, useTrans} from "zographia";
+import {AdditionalProps, Callout, CalloutBody, CalloutIconContainer, GeneralIcon, MultiLineText, useTrans} from "zographia";
+import {Link} from "/client/component/atom/link";
 import {create} from "/client/component/create";
 import {GenerateMyApiKeyForm} from "/client/component/form/generate-my-api-key-form";
 import {useMe} from "/client/hook/auth";
@@ -16,7 +18,7 @@ export const UserDeveloperPart = create(
     className?: string
   } & AdditionalProps): ReactElement | null {
 
-    const {trans} = useTrans("userDeveloperPart");
+    const {trans, transNode} = useTrans("userDeveloperPart");
 
     const me = useMe();
     const {name} = useParams();
@@ -25,6 +27,19 @@ export const UserDeveloperPart = create(
       <div styleName="root" {...rest}>
         <section styleName="section">
           <h3 styleName="heading">{trans("heading.apiKey")}</h3>
+          <MultiLineText styleName="description">
+            {transNode("description.apiKey", {
+              link: (parts) => <Link href="/document/other/api" variant="unstyledUnderline">{parts}</Link>
+            })}
+          </MultiLineText>
+          <Callout styleName="callout">
+            <CalloutIconContainer><GeneralIcon icon={faInfoCircle}/></CalloutIconContainer>
+            <CalloutBody>
+              <MultiLineText is="p">
+                {trans("callout.apiKey")}
+              </MultiLineText>
+            </CalloutBody>
+          </Callout>
           <GenerateMyApiKeyForm/>
         </section>
       </div>
