98669f10fa7c7649071b406f1e3fb1a2f8538ab3
Ziphil <yfe23457@nifty.com>
2025-04-06 20:11:10 +0900

    外部APIのバリデーションが誤っていたのを修正

--- server/external/controller/rest/example.ts
+++ server/external/controller/rest/example.ts
@@ -3,7 +3,7 @@
 import {before, endpoint, restController} from "/server/controller/rest/decorator";
 import {FilledRequest, Request, Response} from "/server/external/controller/rest/base";
 import {checkDictionary, checkMe, limit} from "/server/external/controller/rest/middleware";
-import {validateQuery} from "/server/external/controller/rest/middleware/validate";
+import {validateBody, validateQuery} from "/server/external/controller/rest/middleware/validate";
 import {ExampleCreator, ExampleOfferCreator, ExampleOfferParameterCreator} from "/server/external/creator";
 import {SERVER_PATH_PREFIX} from "/server/external/type/rest";
 import {CustomError, ExampleOfferModel, NormalExampleParameter} from "/server/model";
@@ -43,7 +43,7 @@ export class ExampleExternalRestController extends ExternalRestController {
   }
 
   @endpoint("/v0/dictionary/:identifier/example", "post")
-  @before(checkMe(), limit(), validateQuery("addExample"), checkDictionary("edit"))
+  @before(checkMe(), limit(), validateBody("addExample"), checkDictionary("edit"))
   public async [Symbol()](request: FilledRequest<"addExample", "me" | "dictionary">, response: Response<"addExample">): Promise<void> {
     const {me, dictionary} = request.middlewareBody;
     const {example} = request.body;
@@ -61,7 +61,7 @@ export class ExampleExternalRestController extends ExternalRestController {
   }
 
   @endpoint("/v0/dictionary/:identifier/example/:exampleNumber", "put")
-  @before(checkMe(), limit(), validateQuery("editExample"), checkDictionary("edit"))
+  @before(checkMe(), limit(), validateBody("editExample"), checkDictionary("edit"))
   public async [Symbol()](request: FilledRequest<"editExample", "me" | "dictionary">, response: Response<"editExample">): Promise<void> {
     const {me, dictionary} = request.middlewareBody;
     const exampleNumber = +request.params.exampleNumber;
--- server/external/controller/rest/word.ts
+++ server/external/controller/rest/word.ts
@@ -3,7 +3,7 @@
 import {before, endpoint, restController} from "/server/controller/rest/decorator";
 import {FilledRequest, Response} from "/server/external/controller/rest/base";
 import {checkDictionary, checkMe, limit} from "/server/external/controller/rest/middleware";
-import {validateQuery} from "/server/external/controller/rest/middleware/validate";
+import {validateBody, validateQuery} from "/server/external/controller/rest/middleware/validate";
 import {WordParameterCreator} from "/server/external/creator";
 import {WordCreator} from "/server/external/creator";
 import {SERVER_PATH_PREFIX} from "/server/external/type/rest";
@@ -44,7 +44,7 @@ export class WordExternalRestController extends ExternalRestController {
   }
 
   @endpoint("/v0/dictionary/:identifier/word", "post")
-  @before(checkMe(), limit(), validateQuery("addWord"), checkDictionary("edit"))
+  @before(checkMe(), limit(), validateBody("addWord"), checkDictionary("edit"))
   public async [Symbol()](request: FilledRequest<"addWord", "me" | "dictionary">, response: Response<"addWord">): Promise<void> {
     const {me, dictionary} = request.middlewareBody;
     const {word} = request.body;
@@ -62,7 +62,7 @@ export class WordExternalRestController extends ExternalRestController {
   }
 
   @endpoint("/v0/dictionary/:identifier/word/:wordNumber", "put")
-  @before(checkMe(), limit(), validateQuery("editWord"), checkDictionary("edit"))
+  @before(checkMe(), limit(), validateBody("editWord"), checkDictionary("edit"))
   public async [Symbol()](request: FilledRequest<"editWord", "me" | "dictionary">, response: Response<"editWord">): Promise<void> {
     const {me, dictionary} = request.middlewareBody;
     const wordNumber = +request.params.wordNumber;
