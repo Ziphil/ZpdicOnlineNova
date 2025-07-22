dcf9ad208b2abce711f95921b41ad43c9e9459ad
Ziphil <yfe23457@nifty.com>
2025-07-22 16:22:28 +0900

    リファクタリング

--- client/component/create.ts
+++ client/component/create.ts
@@ -1,26 +1,26 @@
 //
 
-import {FunctionComponent, Ref, forwardRef} from "react";
+import {FunctionComponent, Ref, forwardRef, memo} from "react";
 import cssModules from "react-css-modules";
 
 
-export function create<C extends FunctionComponent<any>>(css: any, component: C): C;
-export function create<C extends FunctionComponent<any>>(css: any, name: string, component: C): C;
-export function create<C extends FunctionComponent<any>>(...args: [any, C] | [any, string, C]): C {
-  let [css, component, name] = (args.length === 2) ? [args[0], args[1]] : [args[0], args[2], args[1]];
+export function create<C extends FunctionComponent<any>>(css: any, name: string, component: C, options?: FunctionComponentOptions): C {
   if (css !== null && css !== undefined) {
     component = cssModules(css.default, {allowMultiple: true, handleNotFoundStyleName: "ignore"})(component);
+  }
+  if (options?.memo) {
+    component = memo(component) as any;
   }
   component.displayName = name ?? "<unknown>";
   return component;
 }
 
-export function createWithRef<C extends FunctionComponentWithRef<any, any>>(css: any, component: C): C;
-export function createWithRef<C extends FunctionComponentWithRef<any, any>>(css: any, name: string, component: C): C;
-export function createWithRef<C extends FunctionComponentWithRef<any, any>>(...args: [any, C] | [any, string, C]): C {
-  let [css, component, name] = (args.length === 2) ? [args[0], args[1]] : [args[0], args[2], args[1]];
+export function createWithRef<C extends FunctionComponentWithRef<any, any>>(css: any, name: string, component: C, options?: FunctionComponentOptions): C {
   if (css !== null && css !== undefined) {
     component = cssModules(component, css.default, {allowMultiple: true, handleNotFoundStyleName: "ignore"});
+  }
+  if (options?.memo) {
+    component = memo(component) as any;
   }
   const forwardedComponent = forwardRef((props, ref) => component({...props, ref}));
   forwardedComponent.displayName = name ?? "<unknown>";
@@ -28,3 +28,4 @@ export function createWithRef<C extends FunctionComponentWithRef<any, any>>(...a
 }
 
 type FunctionComponentWithRef<T, P> = FunctionComponent<P & {ref: Ref<T>}>;
+type FunctionComponentOptions = {memo?: boolean};
