b2e47cbc72f15b72facb3c135a513b6793ed577c
Ziphil <yfe23457@nifty.com>
2026-07-02 22:08:00 +0900

    変更

--- client/util/screenshot.ts
+++ client/util/screenshot.ts
@@ -8,7 +8,6 @@ const SCREENSHOT_PADDING = 12;
 
 export async function saveElementScreenshot(element: HTMLElement, fileName: string): Promise<void> {
   const blob = await toBlob(element, {
-    pixelRatio: 2,
     cacheBust: true,
     backgroundColor: "#FFFFFF",
     width: element.offsetWidth + SCREENSHOT_PADDING * 2,
